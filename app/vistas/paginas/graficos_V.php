8d8c420e1aa6df938ffeab78e0f00c5304f2f063
PabloCabeza <pcabeza7@gmail.com>
2020-07-02 08:17:29 -0400

    Se comienza a seguir en el sistema de control de versiones

---
+++ app/vistas/paginas/graficos_V.php
@@ -0,0 +1,21 @@
+<!-- Se carga el header -->
+<?php include(RUTA_APP . "/vistas/inc/header_V.php"); ?>
+
+		<div class="container contenedor_1">
+                <h1 class="h1_2">Muestra graficos</h1> 
+    
+            <div id="Mostrar_Estadisticas"> 
+                <h1 class="h1_2">Conozca las ordenanzas municipales de su estado</h1>         
+                <p>En esta sección se visualizan graficos</p>
+                <p>Cantidad de denuncias hechas el día de hoy de manera general</p>
+                <p>Cantidad de denuncias por semana de manera general</p>
+                <p>Problemas solucionados</p>
+                <p>Responsables de instituciones en numero</p>
+                <p>Responsabilidad del concejal en numero</p>
+                <p>Responsabilidad del alcalde en numero</p>
+                <p>Responsabilidad de gobernador en numero</p>
+                <p>Responsabilidad del ejecutivo nacional en numeros</p>
+            </div>
+		</div>  
+<!-- Se carga el footer -->
+<?php require(RUTA_APP . "/vistas/inc/footer_V.php"); ?>
