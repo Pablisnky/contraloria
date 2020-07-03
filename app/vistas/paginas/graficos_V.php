4f507efbb683e08cd8dff5a7cfe34d74cc26f5c5
PabloCabeza <pcabeza7@gmail.com>
2020-07-03 12:27:51 -0400

    Se crea el archivo y se comienza a seguir,muestra los detalles de las denuncias realizadas en un servicio en particular

--- app/vistas/paginas/graficos_V.php
+++ app/vistas/paginas/graficos_V.php
@@ -2,9 +2,51 @@
 <?php include(RUTA_APP . "/vistas/inc/header_V.php"); ?>
 
 		<div class="container contenedor_1">
-                <h1 class="h1_2">Muestra graficos</h1> 
-    
-            <div id="Mostrar_Estadisticas"> 
+            <div class="contenedor_15">
+                <h1 class="h1_2">Denuncias realizadas por problemas con <?php echo "Agua potable en los ultimos siete día"?></h1>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">ESTADO</th>
+                            <th scope="col">MUNICIPIO</th>
+                            <th scope="col">DIRECCION</th>
+                            <th scope="col">FECHA</th>
+                            <th scope="col">HORA</th>
+                            <th scope="col">SERVICIO</th>
+                            <th scope="col">PROBLEMA</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                            <?php
+                            //Se reciben los datos desde Estadisticas_C
+                            $arr = $Datos->fetchAll(PDO::FETCH_ASSOC);
+                            foreach($arr as $row) {
+                                $Estado = $row['estado'];
+                                $Municipio = $row['municipio'];
+                                $Direccion = $row['direccion'];
+                                $Fecha = $row['fechaDenuncia'];
+                                $Hora = $row['horaDenuncia'];
+                                $Servicio = $row['servicio'];
+                                $Descripcion = $row['descripcionFallo'];
+                                ?>
+                        <tr>
+                            <td><?php echo $Estado;?></td>
+                            <td><?php echo $Municipio;?></td>
+                            <td><?php echo $Direccion;?></td>
+                            <td><?php echo $Fecha;?></td>
+                            <td><?php echo $Hora;?></td>
+                            <td><?php echo $Servicio;?></td>
+                            <td><?php echo $Descripcion;?></td>
+                        </tr>
+                            <?php } ?>
+                    </tbody>
+                </table>
+                <label class="boton_1">Imprimir reporte</label>
+                <a class="btn btn-primary boton_1" href="javascript: history.go(-2)">Regresar</a>
+             </div>
+		</div>  
+
+            <!-- <div id="Mostrar_Estadisticas"> 
                 <h1 class="h1_2">Conozca las ordenanzas municipales de su estado</h1>         
                 <p>En esta sección se visualizan graficos</p>
                 <p>Cantidad de denuncias hechas el día de hoy de manera general</p>
@@ -15,7 +57,6 @@
                 <p>Responsabilidad del alcalde en numero</p>
                 <p>Responsabilidad de gobernador en numero</p>
                 <p>Responsabilidad del ejecutivo nacional en numeros</p>
-            </div>
-		</div>  
+            </div> -->
 <!-- Se carga el footer -->
 <?php require(RUTA_APP . "/vistas/inc/footer_V.php"); ?>
