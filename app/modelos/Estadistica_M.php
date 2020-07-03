c1f02d4a22fb3dda5f8c08ea5d3fd74c06bf014a
PabloCabeza <pcabeza7@gmail.com>
2020-07-03 11:19:03 -0400

    Las consultas se mejoraron y ahora se realizan con sentencias preparadas

--- app/modelos/Estadistica_M.php
+++ app/modelos/Estadistica_M.php
@@ -1,30 +1,35 @@
 <?php
-    class Estadistica_M{
-        private $db;
+    require(RUTA_APP . "/clases/Conexion_BD.php");
 
-        public function __construct(){
-            //Se conecta a la BD instanciando la clase Conexion_BD
-            $this->db = new Conexion_BD;           
+    class Estadistica_M extends Conexion_BD{
+
+        public function __construct(){ 
+            parent::__construct();  
         }
         
-        public function consultarDatosDenuncia($CodigoDenuncia){
-            $this->db->Consulta("SELECT * FROM fallos INNER JOIN ubicacion ON fallos.aleatorio=ubicacion.aleatorio WHERE ubicacion.aleatorio = '$CodigoDenuncia'");
-            //registros() es un metodo de la clase Conexion_BD
-            $resultados =  $this->db->registros();
-            return $resultados;
-        }
+        // public function consultarDatosDenuncia($CodigoDenuncia){
+        //     $this->db->Consulta("SELECT * FROM fallos INNER JOIN ubicacion ON fallos.aleatorio=ubicacion.aleatorio WHERE ubicacion.aleatorio = '$CodigoDenuncia'");
+        //     //registros() es un metodo de la clase Conexion_BD
+        //     $resultados =  $this->db->registros();
+        //     return $resultados;
+        // }
         
         public function consultarIndicadoresParroquia($Estado, $Municipio, $Parroquia){
-            $this->db->Consulta("SELECT * FROM ubicacion INNER JOIN fallos ON ubicacion.ID_Ubicacion=fallos.ID_Ubicacion WHERE estado = '$Estado' AND municipio = '$Municipio' AND parroquia = '$Parroquia' GROUP BY servicio");
-            //registros() es un metodo de la clase Conexion_BD
-            $resultados =  $this->db->registros();
-            return $resultados;
+            $stmt = $this->dbh->prepare("SELECT * FROM ubicacion INNER JOIN fallos ON ubicacion.ID_Ubicacion=fallos.ID_Ubicacion WHERE estado = :Estado AND municipio = :Municipio AND parroquia = :Parroquia GROUP BY servicio");
+            $stmt->bindValue(':Estado', $Estado, PDO::PARAM_STR);
+            $stmt->bindValue(':Municipio', $Municipio, PDO::PARAM_STR);
+            $stmt->bindValue(':Parroquia', $Parroquia, PDO::PARAM_STR);
+            $stmt->execute();
+            return $stmt;
         } 
 
-        public function consultarCantidadDenuncias($Estado, $Municipio, $Parroquia, $Servicio){
-            $this->db->Consulta("SELECT COUNT(*) AS Total FROM ubicacion INNER JOIN fallos ON ubicacion.ID_Ubicacion=fallos.ID_Ubicacion WHERE estado = '$Estado' AND municipio = '$Municipio' AND parroquia = '$Parroquia' AND servicio = '$Servicio' GROUP BY servicio");
-            //registros() es un metodo de la clase Conexion_BD
-            $resultados =  $this->db->registros();
-            return $resultados;
+        public function consultarCantidadDenuncias($Estado, $Municipio, $Parroquia, $Servicio, $FechaConsulta){
+            $stmt = $this->dbh->query("SELECT COUNT(fechaDenuncia) AS Total FROM ubicacion INNER JOIN fallos ON ubicacion.ID_Ubicacion=fallos.ID_Ubicacion WHERE estado = '$Estado' AND municipio = '$Municipio' AND parroquia = '$Parroquia' AND servicio = '$Servicio' AND fechaDenuncia >= '$FechaConsulta' GROUP BY servicio");      
+            if($stmt->execute()){
+                return $stmt;
+            }
+            else{
+                return false;
+            }
         }
     }
