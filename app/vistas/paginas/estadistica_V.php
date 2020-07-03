ee6c5e390909e3b09fb79c0277b6b295f8039221
PabloCabeza <pcabeza7@gmail.com>
2020-07-03 11:42:52 -0400

    Todos los script js se enviaron al final del archivo, se añadio el contenedor que ocupa toda la altura de la pantalla y deja ver la primera linea del footer, se añadio las opciones para ver la frecuencia de denuncias

--- app/vistas/paginas/estadistica_V.php
+++ app/vistas/paginas/estadistica_V.php
@@ -1,13 +1,10 @@
 <!-- Se carga el header -->
 <?php include(RUTA_APP . "/vistas/inc/header_V.php"); ?>
 
-<script type="text/javascript" src="<?php echo RUTA_URL . '/public/javascript/Municipios.js'?>"></script> 
-<script type="text/javascript" src="<?php echo RUTA_URL . '/public/javascript/parroquias.js'?>"></script> 
-<script type="text/javascript" src="<?php echo RUTA_URL . '/public/javascript/Funciones_Ajax.js'?>"></script> 
-
-		<div class="container contenedor_1">
-            <h1 class="h1_2">Conozca las metricas en servicios publicos de nuestras parroquias</h1> 
-            <form >
+    <div class="container contenedor_1">
+        <div class="contenedor_15">
+            <h1 class="h1_2">Conozca la cantidad de fallos en servicios publicos de su municipio</h1> 
+             <form>
                 <div class="form-row">
                     <div class="col"> 
                         <label for="inputState">Estado</label>
@@ -47,30 +44,51 @@
                     </div>
                     <div class="col">
                         <label for="inputState">Parroquia</label>
-                        <select class="form-control" name="parroquia" id="Parroquia" onchange="llamar_EstadisticasParroquia()">
+                        <select class="form-control" name="parroquia" id="Parroquia" onchange="llamar_EstadisticasParroquia(); mostrarFrecuencia()">
                             <option></option>
                         </select>
                     </div>
                 </div>
+                <div class="contenedor_12" id='Loading'>
+                    <p class="p_6">Espere un momento</p>
+                </div>
+                <div class="contenedor_7" id="Contenedor_7">
+                    <div class="contenedor_14">
+                        <h1 class="h1_3">Denuncias realizadas:</h1>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="frecuencia" value="Semanal" id="Semana" checked onclick="llamar_EstadisticasParroquia()">
+                        <label class="form-check-label label_1" for="Semana" onclick="llamar_EstadisticasParroquia()">Ultimos 7 días</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="frecuencia" value="Quincenal" id="Quince" onclick="llamar_EstadisticasParroquia()">
+                        <label class="form-check-label label_1" for="Quince" onclick="llamar_EstadisticasParroquia()">Ultimos 15 días</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="frecuencia" value="Mensual" id="Treinta"  onclick="llamar_EstadisticasParroquia()">
+                        <label class="form-check-label label_1" for="Treinta" onclick="llamar_EstadisticasParroquia()">Ultimos 30 días</label>
+                    </div>
+                </div>
             </form>
-            <div class="contenedor_66" id='Loading'>
-                <p class="p_6">Espere un momento</p>
-            </div>
-            <div id="Mostrar_Estadisticas">   
-                <h4>Ordenanzas municipales</h4>             
-                <p>En esta sección se visualizan graficos</p>
-                <p>Cantidad de denuncias hechas el día de hoy de manera general</p>
-                <p>Cantidad de denuncias por semana de manera general</p>
-                <p>Problemas solucionados</p>
-                <p>Responsables de instituciones en numero</p>
-                <p>Responsabilidad del concejal en numero</p>
-                <p>Responsabilidad del alcalde en numero</p>
-                <p>Responsabilidad de gobernador en numero</p>
-                <p>Responsabilidad del ejecutivo nacional en numeros</p>
-            </div>
-                
-		</div>  
+            <div id="Mostrar_Estadisticas"></div> 
+        </div>
+    </div>  
+
 <!-- Se carga el footer -->
-<?php 
-    require(RUTA_APP . "/vistas/inc/footer_V.php");
-?>
+<?php require(RUTA_APP . "/vistas/inc/footer_V.php"); ?>
+
+<script type="text/javascript" src="<?php echo RUTA_URL . '/public/javascript/Municipios.js'?>"></script> 
+<script type="text/javascript" src="<?php echo RUTA_URL . '/public/javascript/parroquias.js'?>"></script> 
+<script type="text/javascript" src="<?php echo RUTA_URL . '/public/javascript/Funciones_Ajax.js'?>"></script> 
+
+
+            <!-- <h1 class="h1_2">Conozca las ordenanzas municipales de su estado</h1>         
+            <p>En esta sección se visualizan graficos</p>
+            <p>Cantidad de denuncias hechas el día de hoy de manera general</p>
+            <p>Cantidad de denuncias por semana de manera general</p>
+            <p>Problemas solucionados</p>
+            <p>Responsables de instituciones en numero</p>
+            <p>Responsabilidad del concejal en numero</p>
+            <p>Responsabilidad del alcalde en numero</p>
+            <p>Responsabilidad de gobernador en numero</p>
+            <p>Responsabilidad del ejecutivo nacional en numeros</p> -->
