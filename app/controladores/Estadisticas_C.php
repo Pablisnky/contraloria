1922c94922b78710e27dec268882be6a6031ef12
PabloCabeza <pcabeza7@gmail.com>
2020-07-03 11:04:59 -0400

    Se cambio totalmente el metodo resultados()  Please enter the commit message for your changes. Lines starting

--- app/controladores/Estadisticas_C.php
+++ app/controladores/Estadisticas_C.php
@@ -1,7 +1,12 @@
 <?php
-    // session_start();
+    session_start();
 
     class Estadisticas_C extends Controlador{
+        public $Estado;
+        public $Municipio;
+        public $Parroquia;
+        public $Frecuencia;
+
         public function __construct(){
             //Se accede al servidor de base de datos; Se instancia un objeto correspondiente que se comunica con la BD
             $this->ConsultaEstadistica_M = $this->modelo("Estadistica_M");
@@ -22,15 +27,53 @@
             $this->vista("paginas/estadistica_Indicadores_V", $Datos);
         }
 
-        public function resultados($Estado, $Municipio, $Parroquia){
-            //Se CONSULTA las denuncias
-            $Indicadores = $this->ConsultaEstadistica_M->consultarIndicadoresParroquia($Estado, $Municipio, $Parroquia);
-            // print_r($Indicadores);
+        //Metodo llamado desde Funciones_Ajax.js
+        public function resultados($Estado, $Municipio, $Parroquia, $Frecuencia){
+            $this->Estado = $Estado;
+            $this->Municipio = $Municipio;
+            $this->Parroquia = $Parroquia;
+            $this->Frecuencia = $Frecuencia;
 
-            $Datos=[
-                "informacionDenuncia" => $Indicadores
-            ];
+            $this->Frecuencia($this->Frecuencia);
+        }
 
+        //Este metodo es llamado desde Estadisticas_V/resultados, a la final no era necesario hacerlo, podia estar unido con el metodo cargar() mas abajo y el metodo resultados de arriba 
+        public function Frecuencia($Frecuencia){
+            //Se saca la cuenta de la fecha que se debe mandar a la consulta segun la frecunecia socilicitada (semanal, quincenal, mensual)
+            switch($Frecuencia){
+                case "Semanal": 
+                    //se calcula que fecha era 7 dias atras
+                    $fecha = date('Y-m-d');
+                    $FechaConsulta = strtotime ('-7 day' , strtotime($fecha)) ;
+                    $FechaConsulta = date ('Y-m-j', $FechaConsulta);
+                    $_SESSION['Fecha'] =  $FechaConsulta;
+                    // echo "La sesion es: " . $_SESSION['Fecha'] ;
+                break;
+                case "Quincenal": 
+                    $fecha = date('Y-m-d');
+                    $FechaConsulta = strtotime ('-15 day' , strtotime($fecha)) ;
+                    $FechaConsulta = date ('Y-m-j', $FechaConsulta);
+                    $_SESSION['Fecha'] =  $FechaConsulta;
+                    // echo "La sesion es: " . $_SESSION['Fecha'] ;
+                break;
+                case "Mensual": 
+                    $fecha = date('Y-m-d');
+                    $FechaConsulta = strtotime ('-30 day' , strtotime($fecha)) ;
+                    $FechaConsulta = date ('Y-m-j', $FechaConsulta);
+                    $_SESSION['Fecha'] =  $FechaConsulta;
+                    // echo "La sesion es: " . $_SESSION['Fecha'] ;
+                break;
+            }
+            
+            $this->Cargar($this->Estado, $this->Municipio, $this->Parroquia, $FechaConsulta);
+        }   
+
+        public function Cargar($Estado, $Municipio, $Parroquia, $FechaConsulta){
+            //Se CONSULTA las denuncias para el periodo seleccionado (semanal,quincenal,mensual)
+            $Indicadores = $this->ConsultaEstadistica_M->consultarIndicadoresParroquia($Estado, $Municipio, $Parroquia, $FechaConsulta);
+                      
+            $Datos = $Indicadores ;
+        
             $this->vista("paginas/estadistica_Ind_Parro_V", $Datos);
         }
     }
