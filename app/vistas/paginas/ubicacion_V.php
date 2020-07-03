276ca35883e89f5609dbee8e5e978606e3cc9f4d
PabloCabeza <pcabeza7@gmail.com>
2020-07-03 12:36:54 -0400

    Se inserto el codigo en un bloque DIV para que se muestre a un 100% del alto de la pantalla, dejando un pequeño espacio para ostrar la primera linea del footer

--- app/vistas/paginas/ubicacion_V.php
+++ app/vistas/paginas/ubicacion_V.php
@@ -8,64 +8,66 @@
 </script>
 
 <div class="container contenedor_1"> 
-    <h1 class="h1_2">Ubicación geografica de la comunidad donde existe el falllo con el agua potable</h1>               
-    <form action="<?php echo RUTA_URL?>/Ubicacion_C/recibeUbicacion" method="post">
-        <div class="form-row">
-            <div class="col">
-                <label for="inputState">Estado</label>
-                <select class="form-control" name="estado" onchange="SeleccionarMunicipio(this.form)">
-                    <option></option>
-                    <option>Amazonas</option>
-                    <option>Anzoátegui</option>
-                    <option>Apure</option>
-                    <option>Aragua</option>
-                    <option>Barinas</option>
-                    <option>Bolivar</option>
-                    <option>Carabobo</option>
-                    <option>Cojedes</option>
-                    <option>Delta Amacuro</option>
-                    <option>Distrito Capital</option>
-                    <option>Falcon</option>
-                    <option>Guárico</option>
-                    <option>Lara</option>
-                    <option>Mérida</option>
-                    <option>Miranda</option>
-                    <option>Monagas</option> 
-                    <option>Nueva Esparta</option>
-                    <option>Portuguesa</option>
-                    <option>Sucre</option>
-                    <option>Táchira</option>
-                    <option>Trujillo</option>                           
-                    <option>Vargas</option>
-                    <option>Yaracuy</option>
-                    <option>Zulia</option>
-                </select>
+    <div class="contenedor_17">
+        <h1 class="h1_2">Ubicación geografica de la comunidad donde existe el falllo con el agua potable</h1>               
+        <form action="<?php echo RUTA_URL?>/Ubicacion_C/recibeUbicacion" method="post">
+            <div class="form-row">
+                <div class="col">
+                    <label for="inputState">Estado</label>
+                    <select class="form-control" name="estado" onchange="SeleccionarMunicipio(this.form)">
+                        <option></option>
+                        <option>Amazonas</option>
+                        <option>Anzoátegui</option>
+                        <option>Apure</option>
+                        <option>Aragua</option>
+                        <option>Barinas</option>
+                        <option>Bolivar</option>
+                        <option>Carabobo</option>
+                        <option>Cojedes</option>
+                        <option>Delta Amacuro</option>
+                        <option>Distrito Capital</option>
+                        <option>Falcon</option>
+                        <option>Guárico</option>
+                        <option>Lara</option>
+                        <option>Mérida</option>
+                        <option>Miranda</option>
+                        <option>Monagas</option> 
+                        <option>Nueva Esparta</option>
+                        <option>Portuguesa</option>
+                        <option>Sucre</option>
+                        <option>Táchira</option>
+                        <option>Trujillo</option>                           
+                        <option>Vargas</option>
+                        <option>Yaracuy</option>
+                        <option>Zulia</option>
+                    </select>
+                </div>
+                <div class="col">
+                    <label for="inputState">Municipio</label>
+                    <select class="form-control" name="municipio" onchange="SeleccionarParroquia(this.form)">
+                        <option></option>
+                    </select>
+                </div>
+                <div class="col">
+                    <label for="inputState">Parroquia</label>
+                    <select class="form-control" name="parroquia" onchange="SeleccionarSector(this.form)">
+                        <option></option>
+                    </select>
+                </div>
             </div>
-            <div class="col">
-                <label for="inputState">Municipio</label>
-                <select class="form-control" name="municipio" onchange="SeleccionarParroquia(this.form)">
-                    <option></option>
-                </select>
+            <br>
+            <div class="form-group">
+                <label>Dirección del sector</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" name="direccion" rows="3"></textarea>
             </div>
-            <div class="col">
-                <label for="inputState">Parroquia</label>
-                <select class="form-control" name="parroquia" onchange="SeleccionarSector(this.form)">
-                    <option></option>
-                </select>
+            <div class="">
+                <!-- $Datos es recibido desde Ubicacion_C/index -->
+                <input type="text" name="sector_servicio" value="<?php echo $Datos;?>" hidden >
+                <a class="btn btn-primary boton_1" href="javascript: history.go(-2)">Regresar</a>
+                <input type="submit" class="btn btn-primary boton_1" value="Continuar">
             </div>
-        </div>
-        <br>
-        <div class="form-group">
-            <label>Dirección del sector</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" name="direccion" rows="3"></textarea>
-        </div>
-        <div class="">
-            <!-- $Datos es recibido desde Ubicacion_C/index -->
-            <input type="text" name="sector_servicio" value="<?php echo $Datos;?>" hidden >
-            <a class="btn btn-primary boton_1" href="javascript: history.go(-2)">Regresar</a>
-            <input type="submit" class="btn btn-primary boton_1" value="Continuar">
-        </div>
-    </form>
+        </form>
+    </div>
 </div>  
 
 <!-- Se carga el footer -->
