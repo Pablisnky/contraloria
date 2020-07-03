26d9511b336da9f5d283872dec983d99602e2559
PabloCabeza <pcabeza7@gmail.com>
2020-07-03 11:37:52 -0400

    Se cambio la forma de recibir los datos desde el controlador debido a que ahora se recibi un objeto PDO

--- app/vistas/paginas/estadistica_Ind_Parro_V.php
+++ app/vistas/paginas/estadistica_Ind_Parro_V.php
@@ -1,53 +1,56 @@
-    <div class="container contenedor_1">
+
+<!-- Pagina cargada por el controlador Estadisticas_C/resultados -->
+<div class="container contenedor_16">
+    <?php
+    if(empty($Datos)){
+        echo "Ho hay registro de denuncias en la parroquia indicada";
+    }
+    else{  ?>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">SECTOR</th>
+                    <th scope="col">SERVICIO</th>
+                    <th scope="col">CANTIDAD</th>
+                    <th scope="col">DETALLES</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                //Se reciben los datos desde Estadisticas_C
+                $arr = $Datos->fetchAll(PDO::FETCH_ASSOC);
+                foreach($arr as $row) {
+                    $Sector = $row['sector'];
+                    $Servicio = $row['servicio'];
+                    $Estado = $row['estado'];
+                    $Municipio = $row['municipio'];
+                    $Parroquia = $row['parroquia'];
+                    $FechaConsulta = $_SESSION['Fecha'];
+                    ?>
+                    <tr>
+                        <td><?php echo $Sector?></td>
+                        <td><?php echo $Servicio?></td>
+                        <td>  
+                            <?php
+                            //Se CONSULTA la cantidad (solo numero) de denuncias realizadas por servicio en una parroquia (esta solicitud de consulta debería estar en el controlador, pero no encontre manera de hacerlo)
+                            $this->ConsultaEstadistica_M = $this->modelo("Estadistica_M");
+                            $Denuncias = $this->ConsultaEstadistica_M->consultarCantidadDenuncias($Estado, $Municipio, $Parroquia, $Servicio, $FechaConsulta);
+
+                            $arr_2 = $Denuncias->fetchAll(PDO::FETCH_ASSOC);
+                            foreach($arr_2 as $Denuncias){  
+                                $TotalDenuncias = $Denuncias['Total']; 
+                                echo $TotalDenuncias;
+                            }  
+                            ?>  
+                        </td>  
+                        <td><a href="Graficos_C/index/<?php echo $Estado.','.$Municipio.','.$Parroquia.','.$Servicio?>" target="_blank">Ver</a></td>
+                    </tr>    
+                    <?php 
+                }   
+                ?> 
+            </tbody>
+        </table>
+        <label class="boton_1">Imprimir reporte</label>
         <?php
-        if(empty($Datos)){
-            print_r($Datos);
-            echo "Ho hay registro de denuncias en la parroquia indicada";
-        }
-        else{   ?>
-            <div class="container contenedor_1">
-                <div class="row contenedor_7">
-                    <h3>Denuncias realizadas y sin respuesta oficial</h3>
-                </div>
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">SECTOR</th>
-                            <th scope="col">DETALLE</th>
-                            <th scope="col">CANTIDAD</th>
-                            <th scope="col">DETALLES</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        //Se reciben los datos desde Estadisticas_C
-                        foreach($Datos["informacionDenuncia"] as $Inform_Denun){
-                            $Estado =  $Inform_Denun -> estado; 
-                            $Municipio =  $Inform_Denun -> municipio; 
-                            $Parroquia =  $Inform_Denun -> parroquia; 
-                            $Sector = $Inform_Denun -> sector; 
-                            $Servicio = $Inform_Denun -> servicio; 
-                            $Fecha = $Inform_Denun -> fechaDenuncia;   ?>
-                            <tr>
-                                <td><?php echo $Sector?></td>
-                                <td><?php echo $Servicio?></td>
-                                <td>  
-                                    <?php
-                                    //Se CONSULTA la cantidad de denuncias realizadas por servicio en una parroquia
-                                    $Denuncias = $this->ConsultaEstadistica_M->consultarCantidadDenuncias($Estado, $Municipio, $Parroquia, $Servicio);
-                                    foreach($Denuncias as $Cant_Denun){  
-                                        $TotalDenuncias = $Cant_Denun -> Total; 
-                                        echo $TotalDenuncias;
-                                    }  ?>  
-                                </td>
-                                <td><a class="" href="<?php echo RUTA_URL . '/TotalDenuncia_C/index/Comunidad,AguaServida';?>">Ver</a></td>
-                            </tr>    
-                            <?php 
-                            }   
-                        ?> 
-                    </tbody>
-                </table>
-            </div>
-            <?php
-        }   ?>
-    </div>  
+    }   ?>
+</div>  
