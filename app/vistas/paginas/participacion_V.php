953cd6f7460394abc463d7e963f4f38203a38a2f
PabloCabeza <pcabeza7@gmail.com>
2020-07-03 12:32:44 -0400

    Se edito el texto

--- app/vistas/paginas/participacion_V.php
+++ app/vistas/paginas/participacion_V.php
@@ -9,4 +9,9 @@
             <p>Por ahora solo pueden realizarse 10 denuncias por mes</p>     
             </div>  
 
+            <div>
+                  <p>Si desea agregar una institución publica (ambulatorio, escuela, centro infantil, oficina publica, centro de salud, etc) que no aparezca en los registro puede enviarnos los datos mediante el siguiente formulario</p>
+                  <label>Cargar nueva institución publica</label>
+            </div>
+
  <?php include(RUTA_APP . "/vistas/inc/footer_V.php");?>
