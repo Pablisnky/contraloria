a56a4a8e3d67580a8eafd5c4e83d5ea4e4555c56
PabloCabeza <pcabeza7@gmail.com>
2020-07-03 22:43:18 -0400

    Se coloco un comentario especificando de donde se llama uno de los metodos de la clase Please enter the commit message for your changes. Lines starting

--- app/modelos/Estadistica_M.php
+++ app/modelos/Estadistica_M.php
@@ -23,6 +23,7 @@
             return $stmt;
         } 
 
+        //Consulta solicitada desde estadistica_Ind_Parro_V
         public function consultarCantidadDenuncias($Estado, $Municipio, $Parroquia, $Servicio, $FechaConsulta){
             $stmt = $this->dbh->query("SELECT COUNT(fechaDenuncia) AS Total FROM ubicacion INNER JOIN fallos ON ubicacion.ID_Ubicacion=fallos.ID_Ubicacion WHERE estado = '$Estado' AND municipio = '$Municipio' AND parroquia = '$Parroquia' AND servicio = '$Servicio' AND fechaDenuncia >= '$FechaConsulta' GROUP BY servicio");      
             if($stmt->execute()){
