e224f4a4d73a17ec8f403879d24e9f538763dd0e
PabloCabeza <pcabeza7@gmail.com>
2020-07-05 09:10:42 -0400

    Se realizaron cambios en el HTML

--- app/vistas/paginas/login_V.php
+++ app/vistas/paginas/login_V.php
@@ -4,31 +4,33 @@
 ?>
 
 	<div class="container contenedor_1">
-		<h2 class="h1_1">Para realizar una denuncia es necesario iniciar sesión</h2>
-		<div class="row">
-			<div class="col-sm-4 contenedor_9">
-				<form action="<?php echo RUTA_URL . '/Login_C/ValidarSesion';?>" method="POST">	
-					<div class="form-group">
-						<input class="form-control" type="email" name="correo_Arr" id="Correo" placeholder="e-mail" autocomplete="off">
-					</div>
-					<div class="form-group">
-						<input class="form-control" type="password" name="clave_Arr" id="Clave" placeholder="Contraseña" autocomplete="off">
-					</div>
-					<div class="">
-						<input type="checkbox" id="Recordar" name="recordar" value="1">
-						<label class="recordar" for="Recordar">Recordar datos en este equipo.</label>
-					</div>
-					<input type="text" name="datosVarios" value="<?php echo $Datos;?>" hidden>
-					<input class="btn btn-primary boton_1" type="submit" value="Entrar"><!-- validar_02() se encuentra en return validar_02()validarFormularios.js -->
-					<hr>
-					<p class="p_1">¿Olvidaste tu contraseña?
-					<label class="label_2" onclick="NotificarContrasena()">Recuperala</label></p>
-				</form>
+		<div class="contenedor_15">
+			<h1 class="h1_2">Para realizar una denuncia es necesario iniciar sesión</h1>
+			<div class="row">
+				<div class="col-sm-4 contenedor_9">
+					<form action="<?php echo RUTA_URL . '/Login_C/ValidarSesion';?>" method="POST">	
+						<div class="form-group">
+							<input class="form-control" type="email" name="correo_Arr" id="Correo" placeholder="e-mail" autocomplete="off">
+						</div>
+						<div class="form-group">
+							<input class="form-control" type="password" name="clave_Arr" id="Clave" placeholder="Contraseña" autocomplete="off">
+						</div>
+						<div class="">
+							<input type="checkbox" id="Recordar" name="recordar" value="1">
+							<label class="recordar" for="Recordar">Recordar datos en este equipo.</label>
+						</div>
+						<input type="text" name="datosVarios" value="<?php echo $Datos;?>" hidden>
+						<input class="btn btn-primary boton_1" type="submit" value="Entrar"><!-- validar_02() se encuentra en return validar_02()validarFormularios.js -->
+						<hr>
+						<p class="p_1">¿Olvidaste tu contraseña?
+						<label class="label_4" onclick="NotificarContrasena()">Recuperala</label></p>
+					</form>
+				</div>
 			</div>
-		</div>
-		<div class="">	
-			<h2 class="h1_1">¿No tienes cuenta?
-			<a class="" href="<?php echo RUTA_URL . '/Registro_C';?>">Registrate</a><h2>
+			<div class="">	
+				<p class="p_1">¿No tienes cuenta?
+				<a class="label_4" href="<?php echo RUTA_URL . '/Registro_C';?>">Registrate</a><p>
+			</div>
 		</div>
 		<div class="contenedor_3" id="Contenedor_16" onclick="OcultarDiv()">
 			<form action="<?php echo RUTA_URL . '/Login_C/RecuperarClave';?>" method="POST" autocomplete="off">
