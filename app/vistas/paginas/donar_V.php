cc624115d9235f257c890941a2008e667d2dec83
PabloCabeza <pcabeza7@gmail.com>
2020-07-05 09:03:26 -0400

    Se mejoró el HTML

--- app/vistas/paginas/donar_V.php
+++ app/vistas/paginas/donar_V.php
@@ -1,12 +1,12 @@
 <!-- Se carga el header -->
 <?php include(RUTA_APP . "/vistas/inc/header_V.php"); ?>
 
-		<div class="container contenedor_1">
-        <p>Si deseas donar a la causa puedes realizarlo aportando a la cuenta bancaria Nº</p>
-        <p>Si deseas involucrarte en el proyecto y hacerlo crecer puedes contactarme por los siguientes medios:</p>
-		</div>
-           
-<?php   
-//  Se carga el footer 
-    require(RUTA_APP . "/vistas/inc/footer_V.php");
-?>
+	<div class="container contenedor_1">
+        <div class="contenedor_15">
+            <p>Si deseas donar a la causa puedes realizarlo aportando a la cuenta bancaria Nº</p>
+            <p>Si deseas involucrarte en el proyecto y hacerlo crecer puedes contactarme por los siguientes medios:</p>
+        </div>
+    </div>
+
+    <!-- Se carga el footer -->
+<?php require(RUTA_APP . "/vistas/inc/footer_V.php");?>
