3cbb3f3deba420fbb80089b8795c6bad3a9a46fc
PabloCabeza <pcabeza7@gmail.com>
2020-07-05 09:11:52 -0400

    Se realizaron cambios en el HTML y en la redacción de los textos

--- app/vistas/paginas/nuestroADN_V.php
+++ app/vistas/paginas/nuestroADN_V.php
@@ -2,14 +2,24 @@
 <?php include(RUTA_APP . "/vistas/inc/header_V.php"); ?>
 
 		<div class="container contenedor_1">
-        <p>Los venezolanos hac caido en el letargo de la aceptación, de la esperanza en las acciones de terceros y en la desconfianza de los unos a los otros, se esta tan anegado de coruupción y de malas formas que no se es capaz de distinguir entre la verdad y el error, a titulo personal y con muchos deseos de que nuestro pais sufra una transformación para vivir en una nación transparente y libre de vicios, desarrolle esta plataforma con el deseo y el esfuerzo de impactar positivamente a la sociedad y de motivar a mis compatriotas a buscar la verdad y la transparencia.</p>
+			<h1 class="h1_2">Nuestro ADN</h1>
+            <div class="contenedor_19">
+                <img class="imagen_14 " alt="Fotografia Pablo Cabeza" src="<?php echo RUTA_URL;?>/public/images/Fot_PabloCabeza.jpg">
+                <label class="label_2">Pablo Cabeza</label>
+                <label class="label_3">Programador BackEnd de aplicaciones web</label>
+                <label class="label_3">Desarrollador de software</label>
+                <label class="label_3">T.S.U. Mecánica térmica</label>
+                <!-- <label>CEO horebi.com</label> -->
+            </div>
+            <p class="p_1">El tiempo nos ha demostrado y es un hecho vivido, que los politicos venezolanos acostumbran a negociar las instituciones publicas, administran el patrimonio público en la oscuridad, permiten la degradación de los recursos naturales, utilizan artimañas para trasgedir las leyes, y disfrazan las malas practicas con la compra de voluntades, el populismo, las regalias, el marketing y la psicologia de la manipulación.</p>
 
-        <p>Nuestro sistema politico esta mal diseñado desde el mismisimo comienzo, nada más postularce a una candidatura para dirigir el país se necesitan millones de bolivares, imposible de ser costeados por los candidatos que hasta ahorita han gobernado, los cuales son financiados con el compromiso de entregar una parte de las instituciones publicas al financiador, o peor aún,  utilizar los mismos recursos de la nación para autopostularse en los casos de reelección, ya por allí todo comienza torcido, los candidatos comienzas a adquirir compromisos politicos que serán retribuidos con recompensas provenientes de las arcas de la nación, esto es un hecho conocido, erroneo y aceptado.</p>
+            <p class="p_1">Partiendo de estas caracteristicas generales, como podemos esperar que los politicos resuelvan los problemas del pais, cuando en realidad son parte del problema. Teniendo en cuenta que el desarrollo es obra de grupos de personas organizadas que colocan voluntad, inteligencia, motivación, tenacidad y creatividad en las actividades que emprenden, deberíamos dejar de escuchar promesas politicas particulares y procurar trabajar juntos en nuestras soluciones.</p>
+            
+            <p class="p_1">Este proyecto social lo he llamado <span class="span_1">"Ciudad Transparente"</span>, y plasma con números la realidad que enfrentamos con el tema de los servicios publicos, como ciudadano corriente, aplicar la solución no esta en mis manos, pero puedo ayudar con la ubicación y el seguimiento de un problema especifico de una comunidad, para generar reportes y dejarlos a disponibilidad en esta platforma, asi como tambien desarrollar predicciones con herramientas tecnologicas (Machine learning, Data science) para alertar cuando ciertos asuntos publicos no estan siendo bien administrados.</p>
 
-        <p>Este país necesariamente necesita ser restaurado en su totalidad y el comienzo para hacerlo es el objetivo de esta plataforma, mantenernos informado de lo que esta sucediendo a nuestro alrededor, dar seguimiento a las actividades que se hacen y desarrollar las metricas en machine learning para alertarnos cuando ciertos asuntos publicos no estan siendo bien administrados e irtervenir antes de que los daños se han mayores</p>
+            <p class="p_1">Recuerde, deje de creer que un politico (socialista o capitalista), los marines norteamericanos, los rusos, iraquies o chinos van a solucionar nustros problemas sociales; es usted quién debe sumarse al trabajo e involucrarse en la contraloría social, dejar la dependencia, el interes, el miedo o en otros muchos casos dejar de darsela de vivaracho y oportunista. </p>
 
-        <p>Si deseas donar para el sostenimiento del proyecto puedes realizarlo aportando a la cuenta bancaria Nº</p>
-        <p>Si deseas involucrarte en el proyecto y hacerlo crecer puedes contactarme por los siguientes medios:</p>
+            <p class="p_1">Si deseas involucrarte en el proyecto y hacerlo crecer puedes contactarme por los siguientes medios:</p>
 		</div>
            
 <?php   
