62b2eeda96b44bd771f13ecef1eb8da6888a63a3
PabloCabeza <pcabeza7@gmail.com>
2020-07-05 09:12:54 -0400

    Cambios en el texto del documento

--- app/vistas/paginas/participacion_V.php
+++ app/vistas/paginas/participacion_V.php
@@ -1,17 +1,27 @@
 <!-- Se carga el header -->
 <?php include(RUTA_APP . "/vistas/inc/header_V.php"); ?>
 
-
 		<div class="container contenedor_1">
-            <h2>¿Quiénes pueden hacer uso de la plataforma?</h2>
-            <p>Todo ciudadano venezolano que posea un número de cedula de identidad y se encuentre en el país</p>          
-            <h2>¿Cuantas denuncias se pueden hacer?</h2>
-            <p>Por ahora solo pueden realizarse 10 denuncias por mes</p>     
-            </div>  
+            <h1 class="h1_2">Preguntas frecuentes</h1>
+            <h2 class="h2_2">¿Quiénes pueden hacer uso de la plataforma?</h2>
+            <li>Todo ciudadano que se encuentre en el territorio nacional y que posea un número de cedula venezolana</li>          
+            <li>Todo ciudadano que se encuentre en el extranjero y desee denunciar fallos en Embajadas,  Consulados u otras oficina venezolana ubicada en el exterior</li> 
 
-            <div>
-                  <p>Si desea agregar una institución publica (ambulatorio, escuela, centro infantil, oficina publica, centro de salud, etc) que no aparezca en los registro puede enviarnos los datos mediante el siguiente formulario</p>
-                  <label>Cargar nueva institución publica</label>
-            </div>
+            <h2 class="h2_2">¿Cuantas denuncias se pueden hacer?</h2>
+            <p>Los usuarios registrados pueden realizarse 10 denuncias por mes</p>  
+
+            <h2 class="h2_2">¿Con que frecuencia se puede reportar un fallo?</h2>
+            <p>Una vez reportado un fallo se debe esperar 20 días para volver a reportarlo</p> 
+
+            <h2 class="h2_2">¿Como puedo contribuir a la construcción de la plataforma?</h2>
+            <li>Puede añadir instituciones publicas (ambulatorio, escuela, centro infantil, oficina publica, centro de salud, etc) de su municipio o estado, enviando la información por medio del formulario de adición de institución</li> 
+            <li>Aportar ideas mediante la pizarra de mensajes</li>    
+            
+            <h2 class="h2_2">¿Que estados y municipios estan activados en la plataforma?</h2>
+            <li>Yaracuy - San Felipe, Cuna del proyecto.</li>   
+
+            <h2 class="h2_2">¿Como puede mi municipio aparecer disponible en la plataforma?</h2>
+            <p>Para que un municipio pueda estar disponible, cada sector debe contar con un minimo de cinco especificaciones. Ej: El sector "Deporte" debe contar con dos canchas registrados, una piscina registrada, un estadio registado y un gimnasio registrado; no necesariamente deben ser estas edificaciones, pudiesen ser otras siempre y cuando la suma de todas alcancen un minimo de cinco, esto debe repetirse en cada sector.</p> 
+            </div>  
 
  <?php include(RUTA_APP . "/vistas/inc/footer_V.php");?>
