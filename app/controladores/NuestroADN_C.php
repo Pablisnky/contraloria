be8bdc9c81595e9c78e8ebfe33c1badfcb6e684b
PabloCabeza <pcabeza7@gmail.com>
2020-07-08 11:03:51 -0400

    Se agregaron nuevos metodos que cargan vistas diferentes cada uno, todos los metodos estan relacionados con el item del menu "NuestroADN"

--- app/controladores/NuestroADN_C.php
+++ app/controladores/NuestroADN_C.php
@@ -4,7 +4,24 @@
     class NuestroADN_C extends Controlador{
         //Siempre cargara este metodo por defecto, solo sino se solicita otra metodo, es llamado desde Ubicacion_C/recibeUbicacion
         public function index(){           
-            $this->vista("paginas/nuestroADN_V");
+            $this->vista("paginas/quienesSomos_V");
         }
+
+        public function queHacemos(){
+            $this->vista("paginas/queHacemos_V");
+        }
+
+        public function contactenos(){
+            $this->vista("paginas/contactenos_V");
+        }
+
+        public function pablins_Ky(){
+            $this->vista("paginas/pablins_Ky_V");
+        }
+
+        public function encuesta(){
+            $this->vista("paginas/encuesta_V");
+        }
+        
     }
 ?>    
