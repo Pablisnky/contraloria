af82049b54b1d74d8d7c34f93706274f321f8fe6
PabloCabeza <pcabeza7@gmail.com>
2020-07-08 11:07:28 -0400

    Se genero este nuevo header, es utilizado en las ventanas modales abiertas con windows.open y no contiene los item del menu principal

---
+++ app/vistas/inc/headerEstilos_V.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<title><?php //echo NOMBRESITIO;?></title>
+
+		<meta http-equiv="content-type" content="text/html; charset = utf-8">
+		<meta name="description" content=".">
+		<meta name="keywords" content="">
+		<meta name="author" content="Pablo Cabeza">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta http-equiv="expires" content="27 de diciembre de 2019">
+
+		<meta name="MobileOptimized" content="width">
+        <meta name="HandheldFriendly" content="true">
+ 
+		<link rel="stylesheet" type="text/css" href="<?php echo RUTA_URL?>/css/estilosContraloria.css"/>
+		<link rel='stylesheet' type='text/css' href='https://fonts.googleapis.com/css?family=Raleway:400|Montserrat'>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+		<!-- <link rel="shortcut icon" type="image/png" href="../images/logo_aplicacion/logo_Mira_horebi.png"> -->
+	</head>
+    <body>		
+		<header class="header_1">
+			<nav class="navbar navbar-expand-lg navbar-light menuResponsive  nav_1">
+				<a class="navbar-brand" href="<?php echo RUTA_URL . '/Inicio_C';?>">
+					<img src="<?php echo RUTA_URL;?>/public/images/logo.png" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy"> Ciudad Transparente
+				</a>
+			</nav>
+        </header>
+    </body>
