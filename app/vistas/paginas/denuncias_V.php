80faaf9e2a71f981a26d29f7594511294e8c55ca
PabloCabeza <pcabeza7@gmail.com>
2020-07-08 11:11:50 -0400

    Se generó la pagina de denuncias, en esta aparecen las recomendaciones para hacer una denuncia y los sectores con los servicio que pueden denunciarse, antes esto estaba en la vista de inicio_V

---
+++ app/vistas/paginas/denuncias_V.php
@@ -0,0 +1,141 @@
+<!-- Se carga el header -->
+<?php include(RUTA_APP . "/vistas/inc/header_V.php");  ?>
+
+		<div class="container contenedor_1">
+            <div class="contenedor_8">
+                <div class="row">
+                    <h2 class="h2_2 h2_3">Antes de participar considere los siguientes aspectos:</h2>
+                </div>
+                <div class="row contenedor_7">
+                    <ul>
+                        <li class="li_1">Separe el  aspecto ideológico-partidista  de la  práctica  de  la  contraloría  social  y  de  la  evaluación  de  la  gestión  pública.</li>
+                        <li class="li_1">La contraloría social no debe constituir un “mecanismo para el pase de factura personal".</li>
+                        <li class="li_1"> El control social es un mecanismo de acción en la vigilancia, supervisión, seguimiento y control de los asuntos públicos.</li>
+                        <li class="li_1">Solo puede realizar diez denuncias en el mes.</li>
+                    </ul>     
+                </div>   
+                <div class="row">
+                    <h2 class="h2_2 h2_3">Confidencialidad</h2>
+                </div>
+                <div class="row ">
+                        <p class="p_2">Los datos suministrados a la plataforma son cifrados antes de ser almacenados y no pueden ser rastreados</p>  
+                </div>    
+                <div>
+                    <a class="a_3" href="#up">Selecione un servicio público para realizar su denuncia</a>
+                </div> 
+            </div>
+            <div class="row">
+                <div class="col-sm-4">
+                    <h2 class="bg-primary text-white h2_4">Educación</h2>
+                    <ul class="ul_1">
+                        <li><a class="a_5">Educación inicial</a></li>
+                        <li><a class="a_5">Educación primaria</a></li>
+                        <li><a class="a_5">Educación secundaria</a></li>
+                        <li><a class="a_5">Educación universitaria</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-4">
+                    <h2 class="bg-primary text-white h2_4">Transporte</h2>
+                    <ul class="ul_1">
+                        <li><a class="a_5">Rutas urbanas</a></li>
+                        <li><a class="a_5">Rutas extraurbanas</a></li>
+                        <li><a class="a_5">Terminal de pasajeros</a></li>
+                        <li><a class="a_5">carretera</a></li>
+                        <li><a class="a_5">Autopista</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-4">
+                    <h2 class="bg-primary text-white h2_4">Salud</h2>
+                    <ul class="ul_1">
+                        <li><a class="a_5">CDI</a></li>
+                        <li><a class="a_5">Ambulatorio</a></li>
+                        <li><a class="a_5">Hospital</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-4">
+                    <h2 class="bg-primary text-white h2_4">Comunidad</h2>
+                    <ul class="ul_1">
+                        <li class="label_1"><a class="a_4" href="<?php echo RUTA_URL . '/Ubicacion_C/index/Comunidad,AguaPotable';?>">Agua potable</a></li>
+                        <li class="label_1"><a class="a_4" href="<?php echo RUTA_URL . '/Ubicacion_C/index/Comunidad,AguaServida';?>">Aguas servidas</a></li>
+                        <li class=""><a class="a_5">Electricidad</a></li>
+                        <li class=""><a class="a_5">Gas</a></li>
+                        <li class=""><a class="a_5">Internet</a></li>
+                        <li class=""><a class="a_5">Telefono</a></li>
+                        <li class=""><a class="a_5">Calles</a></li>
+                        <li class=""><a class="a_5">Aseo urbano</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-4">
+                    <h2 class="bg-primary text-white h2_4">Deporte</h2>
+                    <ul class="ul_1">
+                        <li><a class="a_5">Cancha</a></li>
+                        <li><a class="a_5">Gimnasio</a></li>
+                        <li><a class="a_5">Estadium</a></li>
+                        <li><a class="a_5">Piscina</a></li>
+                        <li><a class="a_5">Patinodromo</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-4">
+                    <h2 class="bg-primary text-white h2_4">Cultura</h2>
+                    <ul class="ul_1">
+                        <li><a class="a_5">Teatro</a></li>
+                        <li><a class="a_5">Cinemateca</a></li>
+                        <li><a class="a_5">Librería</a></li>
+                        <li><a class="a_5">Tienda de arte</a></li>
+                        <li><a class="a_5">Museo</a></li>
+                        <li><a class="a_5">Galería</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-4">
+                    <h2 class="bg-primary text-white h2_4">Parque y recreación</h2>
+                    <ul class="ul_1">
+                        <li><a class="a_5">Parque natural</a></li>
+                        <li><a class="a_5">Monumento natural</a></li>
+                        <li><a class="a_5">Parque ejercicio</a></li>
+                        <li><a class="a_5">Plaza</a></li>
+                        <li><a class="a_5">Rio</a></li>
+                        <li><a class="a_5">Playa</a></li>
+
+                    </ul>
+                </div>
+                <div class="col-sm-4">
+                    <h2 class="bg-primary text-white h2_4">Obras publicas</h2>
+                    <ul class="ul_1">
+                        <li><a class="a_5">Construcciones</a></li>
+                        <li><a class="a_5">Remodelaciones</a></li>
+                        <li><a class="a_5">Reparaciones</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-4">
+                    <h2 class="bg-primary text-white h2_4">Instituciones publicas</h2>
+                    <ul class="ul_1">
+                        <li><a class="a_5">Estacion de policia</a></li>
+                        <li><a class="a_5">Estacion de bomberos</a></li>
+                        <li><a class="a_5">Mercado municipal</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-4">
+                    <h2 class="bg-primary text-white h2_4">Entes gubernamentales</h2>
+                    <ul  class="ul_1">
+                        <li><a class="a_5"> Gobernación</a></li>
+                        <li><a class="a_5">Alcaldía</a></li>  
+                        <li><a class="a_5">Notaría</a></li>                  
+                        <li><a class="a_5">Registro mercantil</a></li>
+                        <li><a class="a_5">Registro civil</a></li>
+                        <li><a class="a_5">Notaria</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-4">
+                    <h2 class="bg-primary text-white h2_4">Oficinas en el exterior</h2>
+                    <ul  class="ul_1">
+                        <li><a class="a_5">Embajadas</a></li>
+                        <li><a class="a_5">Consulados</a></li>  
+                        <li><a class="a_5">Otras</a></li>     
+                    </ul>
+                </div>
+            </div> 
+        </div>
+<!-- Se carga el footer -->
+<?php require(RUTA_APP . "/vistas/inc/footer_V.php"); ?>
+
+<script type="text/javascript" src="<?php echo RUTA_URL . '/public/javascript/scrollUp.js';?>"></script>
