a7a16716c8cb0148272ea37de2b30d763f818ca5
PabloCabeza <pcabeza7@gmail.com>
2020-07-10 09:23:06 -0400

    Se cambio el HTML, se añadio la cantidad de denuncias por día y se añadio un grafico con los fallos más comunes en los ultimos treinte días

--- app/vistas/paginas/inicio_V.php
+++ app/vistas/paginas/inicio_V.php
@@ -1,148 +1,45 @@
-<!-- Se carga el header -->
 <?php include(RUTA_APP . "/vistas/inc/header_V.php");  ?>
 
 		<div class="container contenedor_1">
             <div class="contenedor_8">
                 <div class="row contenedor_7">
                     <div class="col-sm-12">
-                        <p class="p_1">La participación ciudadana en el control y evaluación sobre la gestión publica, es necesaria para prevenir la corrupcion, el fraude y la ineficiencia, a la vez que ayuda a los administradores y dirigentes gubernamentales a conocer lo que sucede en sus comunidades y en sus diferentes regiones. Conectar Ambas premisas y realizar metrica con machine learning para prevenir malas practicas en el manejo del asunto público es el objetivo de esta plataforma.</p>
+                        <p class="p_1">La participación ciudadana en el control y evaluación sobre la gestión publica, es necesaria para prevenir la corrupcion, el fraude y la ineficiencia, a la vez que ayuda a los administradores y dirigentes gubernamentales a conocer lo que sucede en sus comunidades. Conectar Ambas premisas, realizar metricas con machine learning y sostener la trazabilidad de los problemas en tiempo real para predecir malas practicas en el manejo del asunto público, es el objetivo de este servicio comunitario.</p>
                     </div>
                 </div>
-                <div class="row">
-                    <h2 class="h2_2 h2_3">Antes de participar considere los siguientes aspectos:</h2>
+                <div class="contenedor_26">
+                    <div class="contenedor_24">
+                        <div class="contenedor_27">
+                            <label class="label_6">Denuncias de hoy</label>
+                            <label class="label_5"><?php echo $Datos;?></label>
+                            <label class="label_3">Municipio San Felipe - Yaracuy</label>
+                        </div>
+                    </div>
+                    <div style="overflow: hidden;">
+                        <iframe src="<?php echo RUTA_URL . '/public/grafico/Gra_MasComunes.php'?>" marginwidth="0" marginheight="0" name="ventana_iframe" frameborder="0" height=300 width=800>
+                        </iframe> 
+                    </div>
                 </div>
-                <div class="row contenedor_7">
-                    <ul>
-                        <li class="li_1">Separe el  aspecto ideológico-partidista  de la  práctica  de  la  contraloría  social  y  de  la  evaluación  de  la  gestión  pública.</li>
-                        <li class="li_1">La contraloría social no debe constituir un “mecanismo para el pase de factura personal".</li>
-                        <li class="li_1"> El control social es un mecanismo de acción en la vigilancia, supervisión, seguimiento y control de los asuntos públicos.</li>
-                        <li class="li_1">Solo puede realizar diez denuncias en el mes.</li>
-                    </ul>     
-                </div>   
-                <div class="row">
-                    <h2 class="h2_2 h2_3">Confidencialidad</h2>
-                </div>
-                <div class="row ">
-                        <p class="p_2">Los datos suministrados a la plataforma son cifrados antes de ser almacenados y no pueden ser rastreados</p>  
-                </div>    
                 <div>
-                    <a class="a_3" href="#up">Selecione un servicio público para realizar su denuncia</a>
+                    <a class="a_3" href="<?php echo RUTA_URL . '/Inicio_C/denuncias';?>">Selecione un servicio público para realizar su denuncia</a>
                 </div> 
             </div>
-            <div class="row">
-                <div class="col-sm-4">
-                    <h2 class="bg-primary text-white h2_4">Educación</h2>
-                    <ul class="ul_1">
-                        <li><a class="a_5">Educación inicial</a></li>
-                        <li><a class="a_5">Educación primaria</a></li>
-                        <li><a class="a_5">Educación secundaria</a></li>
-                        <li><a class="a_5">Educación universitaria</a></li>
-                    </ul>
-                </div>
-                <div class="col-sm-4">
-                    <h2 class="bg-primary text-white h2_4">Transporte</h2>
-                    <ul class="ul_1">
-                        <li><a class="a_5">Rutas urbanas</a></li>
-                        <li><a class="a_5">Rutas extraurbanas</a></li>
-                        <li><a class="a_5">Terminal de pasajeros</a></li>
-                        <li><a class="a_5">carretera</a></li>
-                        <li><a class="a_5">Autopista</a></li>
-                    </ul>
-                </div>
-                <div class="col-sm-4">
-                    <h2 class="bg-primary text-white h2_4">Salud</h2>
-                    <ul class="ul_1">
-                        <li><a class="a_5">CDI</a></li>
-                        <li><a class="a_5">Ambulatorio</a></li>
-                        <li><a class="a_5">Hospital</a></li>
-                    </ul>
-                </div>
-                <div class="col-sm-4">
-                    <h2 class="bg-primary text-white h2_4">Comunidad</h2>
-                    <ul class="ul_1">
-                        <li class="label_1"><a class="a_4" href="<?php echo RUTA_URL . '/Ubicacion_C/index/Comunidad,AguaPotable';?>">Agua potable</a></li>
-                        <li class="label_1"><a class="a_4" href="<?php echo RUTA_URL . '/Ubicacion_C/index/Comunidad,AguaServida';?>">Aguas servidas</a></li>
-                        <li class=""><a class="a_5">Electricidad</a></li>
-                        <li class=""><a class="a_5">Gas</a></li>
-                        <li class=""><a class="a_5">Internet</a></li>
-                        <li class=""><a class="a_5">Telefono</a></li>
-                        <li class=""><a class="a_5">Calles</a></li>
-                        <li class=""><a class="a_5">Aseo urbano</a></li>
-                    </ul>
-                </div>
-                <div class="col-sm-4">
-                    <h2 class="bg-primary text-white h2_4">Deporte</h2>
-                    <ul class="ul_1">
-                        <li><a class="a_5">Cancha</a></li>
-                        <li><a class="a_5">Gimnasio</a></li>
-                        <li><a class="a_5">Estadium</a></li>
-                        <li><a class="a_5">Piscina</a></li>
-                        <li><a class="a_5">Patinodromo</a></li>
-                    </ul>
-                </div>
-                <div class="col-sm-4">
-                    <h2 class="bg-primary text-white h2_4">Cultura</h2>
-                    <ul class="ul_1">
-                        <li><a class="a_5">Teatro</a></li>
-                        <li><a class="a_5">Cinemateca</a></li>
-                        <li><a class="a_5">Librería</a></li>
-                        <li><a class="a_5">Tienda de arte</a></li>
-                        <li><a class="a_5">Museo</a></li>
-                        <li><a class="a_5">Galería</a></li>
-                    </ul>
-                </div>
-                <div class="col-sm-4">
-                    <h2 class="bg-primary text-white h2_4">Parque y recreación</h2>
-                    <ul class="ul_1">
-                        <li><a class="a_5">Parque natural</a></li>
-                        <li><a class="a_5">Monumento natural</a></li>
-                        <li><a class="a_5">Parque ejercicio</a></li>
-                        <li><a class="a_5">Plaza</a></li>
-                        <li><a class="a_5">Rio</a></li>
-                        <li><a class="a_5">Playa</a></li>
-
-                    </ul>
-                </div>
-                <div class="col-sm-4">
-                    <h2 class="bg-primary text-white h2_4">Obras publicas</h2>
-                    <ul class="ul_1">
-                        <li><a class="a_5">Construcciones</a></li>
-                        <li><a class="a_5">Remodelaciones</a></li>
-                        <li><a class="a_5">Reparaciones</a></li>
-                    </ul>
-                </div>
-                <div class="col-sm-4">
-                    <h2 class="bg-primary text-white h2_4">Instituciones publicas</h2>
-                    <ul class="ul_1">
-                        <li><a class="a_5">Estacion de policia</a></li>
-                        <li><a class="a_5">Estacion de bomberos</a></li>
-                        <li><a class="a_5">Mercado municipal</a></li>
-                    </ul>
-                </div>
-                <div class="col-sm-4">
-                    <h2 class="bg-primary text-white h2_4">Entes gubernamentales</h2>
-                    <ul  class="ul_1">
-                        <li><a class="a_5"> Gobernación</a></li>
-                        <li><a class="a_5">Alcaldía</a></li>  
-                        <li><a class="a_5">Notaría</a></li>                  
-                        <li><a class="a_5">Registro mercantil</a></li>
-                        <li><a class="a_5">Registro civil</a></li>
-                        <li><a class="a_5">Notaria</a></li>
-                    </ul>
-                </div>
-                <div class="col-sm-4">
-                    <h2 class="bg-primary text-white h2_4">Oficinas en el exterior</h2>
-                    <ul  class="ul_1">
-                        <li><a class="a_5">Embajadas</a></li>
-                        <li><a class="a_5">Consulados</a></li>  
-                        <li><a class="a_5">Otras</a></li>     
-                    </ul>
-                </div>
-            </div> 
+            <div>
+                <!-- <p>Fallos estadisticamente no resueltos</p>
+                <p>Fallos estadisticamente resueltos</p>
+                
+                <p>En esta sección se visualizan graficos</p>
+                <p>Cantidad de denuncias hechas el día de hoy de manera general</p>
+                <p>Cantidad de denuncias por semana de manera general</p>
+                <p>Problemas solucionados</p>
+                <p>Responsables de instituciones en numero</p>
+                <p>Responsabilidad del concejal en numero</p>
+                <p>Responsabilidad del alcalde en numero</p>
+                <p>Responsabilidad de gobernador en numero</p>
+                <p>Responsabilidad del ejecutivo nacional en numeros</p> -->
+            </div>
         </div>
 <!-- Se carga el footer -->
-<?php 
-    require(RUTA_APP . "/vistas/inc/footer_V.php");
-?>
+<?php require(RUTA_APP . "/vistas/inc/footer_V.php"); ?>
 
 <script type="text/javascript" src="<?php echo RUTA_URL . '/public/javascript/scrollUp.js';?>"></script>
