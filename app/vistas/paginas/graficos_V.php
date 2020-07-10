41f7d6929a0ecc25f81c8a9f65a0e3e439a7138a
PabloCabeza <pcabeza7@gmail.com>
2020-07-10 09:21:22 -0400

    Se añadió la parroquia y la zona a la tabla, y los botoenes historial, infografía.

--- app/vistas/paginas/graficos_V.php
+++ app/vistas/paginas/graficos_V.php
@@ -1,5 +1,4 @@
-<!-- Se carga el header -->
-<?php include(RUTA_APP . "/vistas/inc/header_V.php"); ?>
+<?php include(RUTA_APP . "/vistas/inc/headerEstilos_V.php"); ?>
 
 		<div class="container contenedor_1">
             <div class="contenedor_15">
@@ -9,6 +8,8 @@
                         <tr>
                             <th scope="col">ESTADO</th>
                             <th scope="col">MUNICIPIO</th>
+                            <th scope="col">PARROQUIA</th>
+                            <th scope="col">ZONA</th>
                             <th scope="col">DIRECCION</th>
                             <th scope="col">FECHA</th>
                             <th scope="col">HORA</th>
@@ -18,11 +19,13 @@
                     </thead>
                     <tbody>
                             <?php
-                            //Se reciben los datos desde Estadisticas_C
+                            //Se reciben los datos desde Graficos_C
                             $arr = $Datos->fetchAll(PDO::FETCH_ASSOC);
                             foreach($arr as $row) {
                                 $Estado = $row['estado'];
                                 $Municipio = $row['municipio'];
+                                $Parroquia = $row['parroquia'];
+                                $Zona = $row['zona'];
                                 $Direccion = $row['direccion'];
                                 $Fecha = $row['fechaDenuncia'];
                                 $Hora = $row['horaDenuncia'];
@@ -32,6 +35,8 @@
                         <tr>
                             <td><?php echo $Estado;?></td>
                             <td><?php echo $Municipio;?></td>
+                            <td><?php echo $Parroquia;?></td>
+                            <td><?php echo $Zona;?></td>
                             <td><?php echo $Direccion;?></td>
                             <td><?php echo $Fecha;?></td>
                             <td><?php echo $Hora;?></td>
@@ -41,22 +46,13 @@
                             <?php } ?>
                     </tbody>
                 </table>
-                <label class="boton_1">Imprimir reporte</label>
-                <a class="boton_1 a_2" href="javascript: history.go(-2)">Regresar</a>
+                <div>
+                    <label class="btn btn-primary boton_1" onclick="CerrarPresentacion()">Cerrar</label>
+                    <label class="btn btn-primary boton_1 ">Descargar</label>
+                    <label class="btn btn-primary boton_1"><a class="a_6" href="<?php echo RUTA_URL . '/Infografia_C/index/Comunidad,AguaServida';?>">Infografia</a></label>
+                    <label class="btn btn-primary boton_1"><a class="a_6" href="<?php echo RUTA_URL . '/Infografia_C/index/Comunidad,AguaServida';?>">Historial</a></label>
+                </div>
              </div>
 		</div>  
 
-            <!-- <div id="Mostrar_Estadisticas"> 
-                <h1 class="h1_2">Conozca las ordenanzas municipales de su estado</h1>         
-                <p>En esta sección se visualizan graficos</p>
-                <p>Cantidad de denuncias hechas el día de hoy de manera general</p>
-                <p>Cantidad de denuncias por semana de manera general</p>
-                <p>Problemas solucionados</p>
-                <p>Responsables de instituciones en numero</p>
-                <p>Responsabilidad del concejal en numero</p>
-                <p>Responsabilidad del alcalde en numero</p>
-                <p>Responsabilidad de gobernador en numero</p>
-                <p>Responsabilidad del ejecutivo nacional en numeros</p>
-            </div> -->
-<!-- Se carga el footer -->
 <?php require(RUTA_APP . "/vistas/inc/footer_V.php"); ?>
