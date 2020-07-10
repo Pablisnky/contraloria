0c5fe3660936ae9008d0969fc488fe0d89a403e9
PabloCabeza <pcabeza7@gmail.com>
2020-07-10 09:30:10 -0400

    Se añadió el archivo al proyecto

---
+++ app/vistas/paginas/queHacemos_V.php
@@ -0,0 +1,22 @@
+<!-- Se carga el header -->
+<?php include(RUTA_APP . "/vistas/inc/header_V.php");  ?>
+
+		<div class="container contenedor_1">
+            <div class="contenedor_15">
+                <h1 class="h1_2">¿Porque hacemos y trabajamos en Ciudad Transparente?</h1>
+                <p class="p_1">El tiempo nos ha demostrado con mucho dolor y padecimiento, que los politicos venezolanos acostumbran a negociar las instituciones publicas, administran los recursos economicos de la nación en la oscuridad, permiten la degradación de los recursos naturales, violan las leyes y ocultan los hechos, y disfrazan las malas practicas con la compra de voluntades, el populismo, las regalias, el marketing y la psicologia de la manipulación.</p>
+
+                <p class="p_1">Partiendo de estas caracteristicas generales, como podemos esperar que los politicos resuelvan los problemas del pais, cuando en realidad son parte del problema. Teniendo en cuenta que el desarrollo es obra de grupos de personas organizadas que colocan voluntad, inteligencia, motivación, tenacidad y creatividad en las actividades que emprenden, deberíamos dejar de escuchar promesas politicas particulares y procurar trabajar juntos en nuestras soluciones.</p>
+
+                <p class="p_1">En "Ciudad Transparente" queremos cambiar este ciclo nocivo y encaminarmos hacia un nuevo orden social, donde todos controlemos como van las cuentas y los hechos, que el trabajo publico se haga puertas abiertas; de allí que iniciamos con esta plataforma, que captura las inconformidades de los usuarios de los servicios publicos  para generar reportes estadisticos, historiales de fallas, datos abiertos, xxxxxxxxx y lo más importante, en un momento determinado utilizando tecnicas de Machine learning y Data science pudiesemos prevenir posibles actos de corrupción y fraude a la nación.</p>
+
+                <p class="p_1">Recuerde, deje de creer que un politico (socialista o capitalista), los marines norteamericanos, los rusos, los iraquies o los chinos van a solucionar nuestros problemas sociales; es usted quién debe sumarse al trabajo e involucrarse en la contraloría social, dejar la dependencia, el interes, el miedo o en otros muchos casos dejar de darsela de vivaracho y oportunista. </p>
+                <div class="contenedor_22">
+                </div>
+            </div> 
+        </div>
+
+<!-- Se carga el footer -->
+<?php require(RUTA_APP . "/vistas/inc/footer_V.php"); ?>
+
+<script type="text/javascript" src="<?php echo RUTA_URL . '/public/javascript/scrollUp.js';?>"></script>
