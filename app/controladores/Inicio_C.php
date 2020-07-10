8c51d9d2e520a9bcc6e60da760518691427bde3d
PabloCabeza <pcabeza7@gmail.com>
2020-07-10 08:57:49 -0400

    Se agregó el constructor y el metodo index se añadio una busqueda a la BD para consultar cuantas denuncias se han realizado al día

--- app/controladores/Inicio_C.php
+++ app/controladores/Inicio_C.php
@@ -2,9 +2,22 @@
     session_start();
     
     class Inicio_C extends Controlador{
+        
+        public function __construct(){           
+            //Se accede al servidor de base de datos; Se instancia un objeto correspondiente  que se comunica con la BD 
+            $this->ConsultaInicio_M = $this->modelo("Inicio_M");
+        }
+
         //Siempre cargara este metodo por defecto, solo sino se solicita otra metodo
-        public function index(){
-            $this->vista("paginas/inicio_V");
+        public function index(){            
+            //Se CONSULTA la cantidad de denuncias realiazadas el día de hoy
+            $Denuncias = $this->ConsultaInicio_M->consultarDenunciaDiaria();
+            while($arr = $Denuncias->fetch(PDO::FETCH_ASSOC)){
+                $Num_Deuncias = $arr['Total'];
+                // echo $Num_Deuncias;
+            }
+            
+            $this->vista("paginas/inicio_V", $Num_Deuncias);
         }
         
         //Metodo cargado desde header_V
