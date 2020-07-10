3fecc3e90055c541a5fb7c5dc3cf74f5b2f8da3c
PabloCabeza <pcabeza7@gmail.com>
2020-07-10 09:16:46 -0400

    Se añadio el select zona al formulario, tambien se tuvo que modificar los llamados de las funciones javascript

--- app/vistas/paginas/estadistica_V.php
+++ app/vistas/paginas/estadistica_V.php
@@ -1,5 +1,4 @@
-<!-- Se carga el header -->
-<?php include(RUTA_APP . "/vistas/inc/header_V.php"); ?>
+<?php  include(RUTA_APP . "/vistas/inc/header_V.php"); ?>
 
     <div class="container contenedor_1">
         <div class="contenedor_15">
@@ -44,29 +43,32 @@
                     </div>
                     <div class="col">
                         <label for="inputState">Parroquia</label>
-                        <select class="form-control" name="parroquia" id="Parroquia" onchange="llamar_EstadisticasParroquia(); mostrarFrecuencia()">
+                        <select class="form-control" name="parroquia" id="Parroquia" onchange="SeleccionarSector(this.form)">
                             <option></option>
                         </select>
                     </div>
-                </div>
-                <div class="contenedor_12" id='Loading'>
-                    <p class="p_6">Espere un momento</p>
+                    <div class="col">
+                        <label for="inputState">Sector</label>
+                        <select class="form-control" name="sectores" id="Sector" onchange="llamar_EstadisticasSector(); mostrarFrecuencia()">
+                            <option></option>
+                        </select>
+                    </div>
                 </div>
                 <div class="contenedor_7" id="Contenedor_7">
                     <div class="contenedor_14">
                         <h1 class="h1_3">Denuncias realizadas:</h1>
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="frecuencia" value="Semanal" id="Semana" checked onclick="llamar_EstadisticasParroquia()">
-                        <label class="form-check-label label_1" for="Semana" onclick="llamar_EstadisticasParroquia()">Ultimos 7 días</label>
+                        <input class="form-check-input" type="radio" name="frecuencia" value="Semanal" id="Semana" checked onclick="llamar_EstadisticasSector()">
+                        <label class="form-check-label label_1" for="Semana" onclick="llamar_EstadisticasSector()">Ultimos 7 días</label>
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="frecuencia" value="Quincenal" id="Quince" onclick="llamar_EstadisticasParroquia()">
-                        <label class="form-check-label label_1" for="Quince" onclick="llamar_EstadisticasParroquia()">Ultimos 15 días</label>
+                        <input class="form-check-input" type="radio" name="frecuencia" value="Quincenal" id="Quince" onclick="llamar_EstadisticasSector()">
+                        <label class="form-check-label label_1" for="Quince" onclick="llamar_EstadisticasSector()">Ultimos 15 días</label>
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="frecuencia" value="Mensual" id="Treinta"  onclick="llamar_EstadisticasParroquia()">
-                        <label class="form-check-label label_1" for="Treinta" onclick="llamar_EstadisticasParroquia()">Ultimos 30 días</label>
+                        <input class="form-check-input" type="radio" name="frecuencia" value="Mensual" id="Treinta"  onclick="llamar_EstadisticasSector()">
+                        <label class="form-check-label label_1" for="Treinta" onclick="llamar_EstadisticasSector()">Ultimos 30 días</label>
                     </div>
                 </div>
             </form>
@@ -74,21 +76,4 @@
         </div>
     </div>  
 
-<!-- Se carga el footer -->
 <?php require(RUTA_APP . "/vistas/inc/footer_V.php"); ?>
-
-<script type="text/javascript" src="<?php echo RUTA_URL . '/public/javascript/Municipios.js'?>"></script> 
-<script type="text/javascript" src="<?php echo RUTA_URL . '/public/javascript/parroquias.js'?>"></script> 
-<script type="text/javascript" src="<?php echo RUTA_URL . '/public/javascript/Funciones_Ajax.js'?>"></script> 
-
-
-            <!-- <h1 class="h1_2">Conozca las ordenanzas municipales de su estado</h1>         
-            <p>En esta sección se visualizan graficos</p>
-            <p>Cantidad de denuncias hechas el día de hoy de manera general</p>
-            <p>Cantidad de denuncias por semana de manera general</p>
-            <p>Problemas solucionados</p>
-            <p>Responsables de instituciones en numero</p>
-            <p>Responsabilidad del concejal en numero</p>
-            <p>Responsabilidad del alcalde en numero</p>
-            <p>Responsabilidad de gobernador en numero</p>
-            <p>Responsabilidad del ejecutivo nacional en numeros</p> -->
