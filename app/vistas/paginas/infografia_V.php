e0b3c0296099f3d04b7127c3bc397c0d2d9e3256
PabloCabeza <pcabeza7@gmail.com>
2020-07-10 09:25:13 -0400

    Se añadio el archivo al proyecto, hasta ahora esta vacio

---
+++ app/vistas/paginas/infografia_V.php
@@ -0,0 +1,26 @@
+<!-- Se carga el header -->
+<?php include(RUTA_APP . "/vistas/inc/header_V.php"); ?>
+
+		<div class="container contenedor_1">
+            <div class="contenedor_15">
+                <h1 class="h1_2">Infografia</h1>
+                <div>
+                    <label class="btn btn-primary boton_1" onclick="CerrarPresentacion()">Cerrar</label>
+                    <label class="btn btn-primary boton_1 ">Descargar</label>
+                </div>
+             </div>
+		</div>  
+
+            <!-- <div id="Mostrar_Estadisticas">         
+                <p>En esta sección se visualizan graficos</p>
+                <p>Cantidad de denuncias hechas el día de hoy de manera general</p>
+                <p>Cantidad de denuncias por semana de manera general</p>
+                <p>Problemas solucionados</p>
+                <p>Responsables de instituciones en numero</p>
+                <p>Responsabilidad del concejal en numero</p>
+                <p>Responsabilidad del alcalde en numero</p>
+                <p>Responsabilidad de gobernador en numero</p>
+                <p>Responsabilidad del ejecutivo nacional en numeros</p>
+            </div> -->
+<!-- Se carga el footer -->
+<?php require(RUTA_APP . "/vistas/inc/footer_V.php"); ?>
