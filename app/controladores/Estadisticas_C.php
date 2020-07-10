e5132a41f1e9d5c22052e54b8da55b6f211703bd
PabloCabeza <pcabeza7@gmail.com>
2020-07-10 08:55:48 -0400

    Se añadio el parametro zona para dar una ubicacion más especifica de las denuncias

--- app/controladores/Estadisticas_C.php
+++ app/controladores/Estadisticas_C.php
@@ -18,10 +18,11 @@
         }
 
         //Metodo llamado desde Funciones_Ajax.js
-        public function resultados($Estado, $Municipio, $Parroquia, $Frecuencia){
+        public function resultados($Estado, $Municipio, $Parroquia,  $Zona, $Frecuencia){
             $this->Estado = $Estado;
             $this->Municipio = $Municipio;
             $this->Parroquia = $Parroquia;
+            $this->Zona = $Zona;
             $this->Frecuencia = $Frecuencia;
 
             $this->Frecuencia($this->Frecuencia);
@@ -31,38 +32,39 @@
         public function Frecuencia($Frecuencia){
             //Se saca la cuenta de la fecha que se debe mandar a la consulta segun la frecunecia socilicitada (semanal, quincenal, mensual)
             switch($Frecuencia){
-                case "Semanal": 
+                case 'Semanal': 
                     //se calcula que fecha era 7 dias atras
                     $fecha = date('Y-m-d');
                     $FechaConsulta = strtotime ('-7 day' , strtotime($fecha)) ;
                     $FechaConsulta = date ('Y-m-j', $FechaConsulta);
                     $_SESSION['Fecha'] =  $FechaConsulta;
-                    // echo "La sesion es: " . $_SESSION['Fecha'] ;
+                    // echo 'La sesion es: ' . $_SESSION['Fecha'] ;
                 break;
-                case "Quincenal": 
+                case 'Quincenal': 
                     $fecha = date('Y-m-d');
                     $FechaConsulta = strtotime ('-15 day' , strtotime($fecha)) ;
                     $FechaConsulta = date ('Y-m-j', $FechaConsulta);
                     $_SESSION['Fecha'] =  $FechaConsulta;
-                    // echo "La sesion es: " . $_SESSION['Fecha'] ;
+                    // echo 'La sesion es: ' . $_SESSION['Fecha'] ;
                 break;
-                case "Mensual": 
+                case 'Mensual': 
                     $fecha = date('Y-m-d');
                     $FechaConsulta = strtotime ('-30 day' , strtotime($fecha)) ;
                     $FechaConsulta = date ('Y-m-j', $FechaConsulta);
                     $_SESSION['Fecha'] =  $FechaConsulta;
-                    // echo "La sesion es: " . $_SESSION['Fecha'] ;
+                    // echo 'La sesion es: ' . $_SESSION['Fecha'] ;
                 break;
             }
-            $this->Cargar($this->Estado, $this->Municipio, $this->Parroquia, $FechaConsulta);
+                
+            $this->Cargar($this->Estado, $this->Municipio, $this->Parroquia, $this->Zona, $FechaConsulta);
         }   
 
-        public function Cargar($Estado, $Municipio, $Parroquia, $FechaConsulta){
+        public function Cargar($Estado, $Municipio, $Parroquia, $Zona, $FechaConsulta){
             //Se CONSULTA las denuncias para el periodo seleccionado (semanal,quincenal,mensual)
-            $Indicadores = $this->ConsultaEstadistica_M->consultarIndicadoresParroquia($Estado, $Municipio, $Parroquia, $FechaConsulta);
-                      
+            $Indicadores = $this->ConsultaEstadistica_M->consultarIndicadoresParroquia($Estado, $Municipio, $Parroquia, $Zona, $FechaConsulta);
+            
             $Datos = $Indicadores ;
-        
+
             $this->vista("paginas/estadistica_Ind_Parro_V", $Datos);
         }
     }
