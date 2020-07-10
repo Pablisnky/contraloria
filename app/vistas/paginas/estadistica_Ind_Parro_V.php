cca72d4661e19f5d85af4265fbea057106bee621
PabloCabeza <pcabeza7@gmail.com>
2020-07-10 09:15:33 -0400

    Se agrego la zona de la parroquia a los criterios de busquedas en la BD

--- app/vistas/paginas/estadistica_Ind_Parro_V.php
+++ app/vistas/paginas/estadistica_Ind_Parro_V.php
@@ -5,7 +5,7 @@
     if(empty($Datos)){
         echo "Ho hay registro de denuncias en la parroquia indicada";
     }
-    else{  ?>
+    else{ ?>
         <table class="table">
             <thead>
                 <tr>
@@ -19,35 +19,35 @@
                 <?php
                 //Se reciben los datos desde Estadisticas_C/Cargar
                 $arr = $Datos->fetchAll(PDO::FETCH_ASSOC);
-                foreach($arr as $row){
-                    $Sector = $row['sector'];
-                    $Servicio = $row['servicio'];
-                    $Estado = $row['estado'];
-                    $Municipio = $row['municipio'];
-                    $Parroquia = $row['parroquia'];
-                    $FechaConsulta = $_SESSION['Fecha'];
-                    ?>
-                    <tr>
-                        <td><?php echo $Sector?></td>
-                        <td><?php echo $Servicio?></td>
-                        <td>  
-                            <?php
-                            //Se CONSULTA la cantidad (solo numero) de denuncias realizadas por servicio en una parroquia (esta solicitud de consulta debería estar en el controlador, pero no encontre manera de hacerlo)
-                            $this->ConsultaEstadistica_M = $this->modelo("Estadistica_M");
-                            $Denuncias = $this->ConsultaEstadistica_M->consultarCantidadDenuncias($Estado, $Municipio, $Parroquia, $Servicio, $FechaConsulta);
+                    foreach($arr as $row){
+                        $Sector = $row['sector'];
+                        $Servicio = $row['servicio'];
+                        $Estado = $row['estado'];
+                        $Municipio = $row['municipio'];
+                        $Parroquia = $row['parroquia'];
+                        $Zona = $row['zona'];
+                        $FechaConsulta = $_SESSION['Fecha'];
+                        ?>
+                        <tr>
+                            <td><?php echo $Sector?></td>
+                            <td><?php echo $Servicio?></td>
+                            <td>  
+                                <?php
+                                //Se CONSULTA la cantidad (solo numero) de denuncias realizadas por servicio en una parroquia (esta solicitud de consulta debería estar en el controlador, pero no encontre manera de hacerlo)
+                                $this->ConsultaEstadistica_M = $this->modelo("Estadistica_M");
+                                $Denuncias = $this->ConsultaEstadistica_M->consultarCantidadDenuncias($Estado, $Municipio, $Parroquia, $Zona, $Servicio, $FechaConsulta);
 
-                            $arr_2 = $Denuncias->fetchAll(PDO::FETCH_ASSOC);
-                            foreach($arr_2 as $Denuncias){  
-                                $TotalDenuncias = $Denuncias['Total']; 
-                                echo $TotalDenuncias;
-                            }                          
-                            ?>  
-                            
-                        </td>  
-                        <td><label class="label_4" onclick="AbrirPresentacion('<?php echo $Estado?>', '<?php echo $Municipio?>','<?php echo $Parroquia?>','<?php echo $FechaConsulta?>','<?php echo $Servicio?>')">Ver</label></td>
-                    </tr>    
-                    <?php 
-                }   
+                                $arr_2 = $Denuncias->fetchAll(PDO::FETCH_ASSOC);
+                                foreach($arr_2 as $Denuncias){  
+                                    $TotalDenuncias = $Denuncias['Total']; 
+                                    echo $TotalDenuncias;
+                                }                          
+                                ?>                                  
+                            </td>  
+                            <td><label class="label_4" onclick="AbrirPresentacion('<?php echo $Estado?>', '<?php echo $Municipio?>','<?php echo $Parroquia?>','<?php echo $FechaConsulta?>','<?php echo $Servicio?>')">Ver</label></td>
+                        </tr>    
+                        <?php 
+                    }  
                 ?> 
             </tbody>
         </table>
