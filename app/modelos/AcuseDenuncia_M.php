2233b81a88c6dc4aeee00ea70dd74ed9edffc6c2
PabloCabeza <pcabeza7@gmail.com>
2020-07-10 09:01:46 -0400

    Se agregó la zona de la parroquia a los datos que se almacenarán en la BD

--- app/modelos/AcuseDenuncia_M.php
+++ app/modelos/AcuseDenuncia_M.php
@@ -7,13 +7,14 @@
             parent::__construct();       
         }
 
-        public function consultarCodigoDenuncia($ID_Afiliado, $CodigoFallo, $Estado, $Municipio, $Parroquia){
-            $stmt = $this->dbh->prepare("SELECT * FROM fallos WHERE ID_Afiliado = :id_afiliado AND (codigoFallo = :codigo_fallo && :codigo_fallo != 2000) AND ID_Ubicacion = ANY (SELECT ID_Ubicacion FROM ubicacion WHERE  estado = :estado AND municipio = :municipio AND parroquia = :parroquia)");
-            $stmt->bindValue(':id_afiliado', $ID_Afiliado, PDO::PARAM_INT);
-            $stmt->bindValue(':codigo_fallo', $CodigoFallo, PDO::PARAM_INT);
-            $stmt->bindValue(':estado', $Estado, PDO::PARAM_STR);
-            $stmt->bindValue(':municipio', $Municipio, PDO::PARAM_STR);
-            $stmt->bindValue(':parroquia', $Parroquia, PDO::PARAM_STR);
+        public function consultarCodigoDenuncia($ID_Afiliado, $CodigoFallo, $Estado, $Municipio, $Parroquia, $Zona){
+            $stmt = $this->dbh->prepare("SELECT * FROM fallos WHERE ID_Afiliado = :id_afiliado AND (codigoFallo = :codigo_fallo && :codigo_fallo != 2000) AND ID_Ubicacion = ANY (SELECT ID_Ubicacion FROM ubicacion WHERE  estado = :estado AND municipio = :municipio AND parroquia = :parroquia AND zona = :zona)");
+            $stmt->bindParam(':id_afiliado', $ID_Afiliado, PDO::PARAM_INT);
+            $stmt->bindParam(':codigo_fallo', $CodigoFallo, PDO::PARAM_INT);
+            $stmt->bindParam(':estado', $Estado, PDO::PARAM_STR);
+            $stmt->bindParam(':municipio', $Municipio, PDO::PARAM_STR);
+            $stmt->bindParam(':parroquia', $Parroquia, PDO::PARAM_STR);
+            $stmt->bindParam(':zona', $Zona, PDO::PARAM_STR);
             $stmt->execute();
             return $stmt->rowCount();
         }
@@ -27,19 +28,21 @@
         }
         
         public function insertarUbicacion($RecibeVarios, $Aleatorio){    
-            $stmt = $this->dbh->prepare("INSERT INTO ubicacion(estado, municipio, parroquia, direccion, aleatorio) VALUES (:ESTADO, :MUNICIPIO, :PARROQUIA, :DIRECCION, :ALEATORIO_U)");
+            $stmt = $this->dbh->prepare("INSERT INTO ubicacion(estado, municipio, parroquia, zona, direccion, aleatorio) VALUES (:ESTADO, :MUNICIPIO, :PARROQUIA, :ZONA, :DIRECCION, :ALEATORIO_U)");
 
             //Se vinculan los valores de las sentencias preparadas
             $stmt->bindParam(':ESTADO', $estado);
             $stmt->bindParam(':MUNICIPIO', $municipio);
             $stmt->bindParam(':PARROQUIA', $parroquia);
+            $stmt->bindParam(':ZONA', $zona);
             $stmt->bindParam(':DIRECCION', $direccion);
             $stmt->bindParam(':ALEATORIO_U', $aleatorio_u);
             
             $estado = $RecibeVarios[1];
             $municipio = $RecibeVarios[2];
             $parroquia = $RecibeVarios[3];
-            $direccion = $RecibeVarios[4];
+            $zona = $RecibeVarios[4];
+            $direccion = $RecibeVarios[5];
             $aleatorio_u = $Aleatorio;
                         
             //Se ejecuta la inserción de los datos en la tabla
@@ -58,8 +61,8 @@
             return $stmt;
         }
 
-        public function insertarDescripcionDenuncia($Aleatorio, $ID_Afiliado, $ID_Ubicacion, $RecibeVarios, $FechaCaducidad){      
-            $stmt = $this->dbh->prepare("INSERT INTO fallos(ID_Ubicacion, ID_Afiliado, aleatorio, sector, servicio, codigoFallo, descripcionFallo, abierto, fechaDenuncia, fechaCaducidad) VALUES (:ID_UBICACION, :ID_AFILIADO, :ALEATORIO, :SECTOR, :SERVICIO, :CODIGO_FALLO, :DESCRIPCION, :ABIERTO, NOW(), :FECHA_CADUCIDAD)");
+        public function insertarDescripcionDenuncia($Aleatorio, $ID_Afiliado, $ID_Ubicacion, $RecibeVarios, $FechaCaducidad, $Hora){      
+            $stmt = $this->dbh->prepare("INSERT INTO fallos(ID_Ubicacion, ID_Afiliado, aleatorio, sector, servicio, codigoFallo, descripcionFallo, abierto, fechaDenuncia, fechaCaducidad, horaDenuncia) VALUES (:ID_UBICACION, :ID_AFILIADO, :ALEATORIO, :SECTOR, :SERVICIO, :CODIGO_FALLO, :DESCRIPCION, :ABIERTO, NOW(), :FECHA_CADUCIDAD, :HORA)");
 
             $stmt->bindParam(':ID_UBICACION', $id_ubicacion);
             $stmt->bindParam(':ID_AFILIADO', $id_afiliado);
@@ -70,16 +73,18 @@
             $stmt->bindParam(':DESCRIPCION', $descripcion);
             $stmt->bindParam(':ABIERTO', $abierto);
             $stmt->bindParam(':FECHA_CADUCIDAD', $fecha_caducidad);
+            $stmt->bindParam(':HORA', $hora);
 
             $id_ubicacion = $ID_Ubicacion;
             $id_afiliado = $ID_Afiliado;
             $aleatorio = $Aleatorio;
-            $sector = $RecibeVarios[5];
-            $servicio = $RecibeVarios[6];
+            $sector = $RecibeVarios[6];
+            $servicio = $RecibeVarios[7];
             $codigo_fallo = $RecibeVarios[0];
-            $descripcion = $RecibeVarios[7];
+            $descripcion = $RecibeVarios[8];
             $abierto = 1;
             $fecha_caducidad = $FechaCaducidad;
+            $hora = $Hora;
 
             //Se ejecuta la inserción de los datos en la tabla
             if($stmt->execute()){
