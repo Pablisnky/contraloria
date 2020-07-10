9e3892ce16ad88946f34eb01192d6dff27c6220c
PabloCabeza <pcabeza7@gmail.com>
2020-07-10 09:34:40 -0400

    Se añadió el criterio Zona a la busqueda.

--- public/javascript/Funciones_Ajax.js
+++ public/javascript/Funciones_Ajax.js
@@ -30,43 +30,42 @@ var http_request = false;
         } 
 
 //-------------------------------------------------------------------------------------------------
-//-------------------------------------------------------------------------------------------------
-    function llamar_EstadisticasParroquia(){//Es llamada desde Estadistica_V.php
-        document.getElementById('Loading').style.display = 'block';
+    function llamar_EstadisticasSector(){//Es llamada desde Estadistica_V.php
+        // document.getElementById('Mostrar_Estadisticas').style.display = 'block';
         let A = document.getElementById("Estado").value;
         let B = document.getElementById("Municipio").value;
         let C = document.getElementById("Parroquia").value;
+        let D = document.getElementById("Sector").value;
         let porNombre = document.getElementsByName("frecuencia");
         //Se recorren todos los valores del radio button para encontrar el seleccionado
         for(var i=0; i<porNombre.length; i++){
             if(porNombre[i].checked){
-                D= porNombre[i].value;
+                E= porNombre[i].value;
             }
         }
-        var url="estadisticas_C/resultados/" + A + "/" + B + "/" + C + "/" + D;
+        var url="estadisticas_C/resultados/" + A + "/" + B + "/" + C + "/" + D + "/" + E;
         http_request.open('GET',url,true); 
         //Cuando XMLHttpRequest cambie de estado, ejecutamos esta funcion    
-        peticion.onreadystatechange = respuesta_EstadisticasParroquia;
+        peticion.onreadystatechange = respuesta_EstadisticasSector;
         peticion.setRequestHeader("content-type","application/x-www-form-urlencoded");
         // Enviamos los datos con el metodo send
         peticion.send("null");
     }                                                           
-    function respuesta_EstadisticasParroquia(){
+    function respuesta_EstadisticasSector(){
         if(peticion.readyState == 4){
             if(peticion.status == 200){
                 document.getElementById('Mostrar_Estadisticas').innerHTML=peticion.responseText;
-                document.getElementById('Loading').style.display = 'none';
+                // document.getElementById('Loading').style.display = 'none';
             } 
             else{
                 alert('Hubo problemas con la petición.');
             }
         }
         else{ //en caso contrario, mostramos un gif simulando una precarga
-            document.getElementById('Mostrar_evaluar').innerHTML='Cargando registros';
+            document.getElementById('Mostrar_Estadisticas').innerHTML='Cargando registros';
         }
     }
     
-//-------------------------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------------------------
 
 // function llamar_IndicadoresSemanal(){//Es llamada desde Estadistica_Ind_Parro_V.php
