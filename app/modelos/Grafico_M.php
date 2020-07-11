4c1f74c4084400f1e0a3f6163501fec37a3afb0d
PabloCabeza <pcabeza7@gmail.com>
2020-07-11 18:04:53 -0400

    - Se añadio la zona a la consulta consultarDetallesIndicadores

--- app/modelos/Grafico_M.php
+++ app/modelos/Grafico_M.php
@@ -7,11 +7,12 @@
             parent::__construct();  
         }
                 
-        public function consultarDetallesIndicadores($Estado, $Municipio, $Parroquia, $Servicio,  $FechaConsulta){
-            $stmt = $this->dbh->prepare("SELECT * FROM ubicacion INNER JOIN fallos ON ubicacion.ID_Ubicacion=fallos.ID_Ubicacion WHERE estado = :Estado AND municipio = :Municipio AND parroquia = :Parroquia AND servicio = :Servicio AND fechaDenuncia >= :fechaDenuncia");
+        public function consultarDetallesIndicadores($Estado, $Municipio, $Parroquia, $Zona,$FechaConsulta, $Servicio){
+            $stmt = $this->dbh->prepare("SELECT * FROM ubicacion INNER JOIN fallos ON ubicacion.ID_Ubicacion=fallos.ID_Ubicacion INNER JOIN descripcionfallo ON fallos.codigoFallo=descripcionfallo.codigoFallo WHERE estado = :Estado AND municipio = :Municipio AND parroquia = :Parroquia AND zona = :Zona AND servicio = :Servicio AND fechaDenuncia >= :fechaDenuncia");
             $stmt->bindValue(':Estado', $Estado, PDO::PARAM_STR);
             $stmt->bindValue(':Municipio', $Municipio, PDO::PARAM_STR);
             $stmt->bindValue(':Parroquia', $Parroquia, PDO::PARAM_STR);
+            $stmt->bindValue(':Zona', $Zona, PDO::PARAM_STR);
             $stmt->bindValue(':Servicio', $Servicio, PDO::PARAM_STR);
             $stmt->bindValue(':fechaDenuncia', $FechaConsulta, PDO::PARAM_STR);
             $stmt->execute();
