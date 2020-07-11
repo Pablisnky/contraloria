6ee342e96647c9d590d4a6fef254b70e96b6a7cb
PabloCabeza <pcabeza7@gmail.com>
2020-07-11 17:31:02 -0400

    Se quito un comentario irrelevante y se ordeno el código

--- app/controladores/Estadisticas_C.php
+++ app/controladores/Estadisticas_C.php
@@ -8,17 +8,15 @@
         public $Frecuencia;
 
         public function __construct(){
-            //Se accede al servidor de base de datos; Se instancia un objeto correspondiente que se comunica con la BD
             $this->ConsultaEstadistica_M = $this->modelo("Estadistica_M");
         }
 
-        //Siempre cargara este metodo por defecto, solo sino se solicita otra metodo, los parametros los recibe de inicio_V.php
         public function index(){
             $this->vista("paginas/estadistica_V");
         }
 
         //Metodo llamado desde Funciones_Ajax.js
-        public function resultados($Estado, $Municipio, $Parroquia,  $Zona, $Frecuencia){
+        public function resultados($Estado, $Municipio, $Parroquia, $Zona, $Frecuencia){
             $this->Estado = $Estado;
             $this->Municipio = $Municipio;
             $this->Parroquia = $Parroquia;
