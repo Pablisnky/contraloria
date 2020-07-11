ad75dbdcc6e7965dfb16b1ef22e60baa7896ab77
PabloCabeza <pcabeza7@gmail.com>
2020-07-11 17:39:50 -0400

    Se añadioa el servicio a la informacion que se extrae de la BD, tambie se n se decidio hacer el recorrido FETCH_ASSOC en este archivo y no en la vista.Ahora en la vista solo se hace un foreach

--- app/controladores/Graficos_C.php
+++ app/controladores/Graficos_C.php
@@ -4,26 +4,53 @@
     class Graficos_C extends Controlador{
 
         public function __construct(){
-            //Se accede al servidor de base de datos; Se instancia un objeto correspondiente que se comunica con la BD
+            //Se instancia un objeto que se comunica con la BD
             $this->ConsultaGrafico_M = $this->modelo("Grafico_M");
         }
 
-        //Siempre cargara este metodo por defecto, solo sino se solicita otra metodo, los parametros los recibe de Funciones_varias.js por medio de la función AbrirPresentacion()
+        //solo sino se solicita otra metodo, los parametros los recibe de Funciones_varias.js por medio de la función AbrirPresentacion()
         public function index($DatosAgrupados){
             //$DatosAgrupados contiene una cadena con las variable estado, municipio, parroquia, servicio y fecha separados por coma, se convierte en array para separar los elementos
             $DatosAgrupados = explode(",", $DatosAgrupados);
+            // print_r($DatosAgrupados);
+            
             $Estado = $DatosAgrupados[0];
             $Municipio = $DatosAgrupados[1];
             $Parroquia = $DatosAgrupados[2];
-            $Servicio = $DatosAgrupados[3];
+            $Zona = $DatosAgrupados[3];
             $FechaConsulta = $DatosAgrupados[4];
+            $Servicio = $DatosAgrupados[5];
 
             //Se CONSULTA las denuncias para el periodo seleccionado (semanal,quincenal,mensual)
-            $Indicadores_2 = $this->ConsultaGrafico_M->consultarDetallesIndicadores($Estado, $Municipio, $Parroquia, $Servicio, $FechaConsulta);
+            $Indicadores_2 = $this->ConsultaGrafico_M->consultarDetallesIndicadores($Estado, $Municipio, $Parroquia, $Zona, $FechaConsulta, $Servicio);
 
             $Datos = $Indicadores_2;
+            
+            $Datos = $Datos->fetchAll(PDO::FETCH_ASSOC);
+            foreach($Datos as $row){
+                $Estado = $row['estado'];
+                $Municipio = $row['municipio'];
+                $Parroquia = $row['parroquia'];
+                $Zona = $row['zona'];
+                $Direccion = $row['direccion'];
+                $Fecha = $row['fechaDenuncia'];
+                $Hora = $row['horaDenuncia'];
+                $Servicio = $row['servicio'];
+                $Descripcion = $row['descripcionFallo'];
+            }
 
-            $this->vista("paginas/graficos_V",$Datos);
+            //Se establece el tiempo de la denuncia segun su fecha
+            // if($Fecha >= $FechaTope){
+            //     $Frecuencia = "siete";
+            // }
+            // else if(){
+            //     $Frecuencia = "quince";
+            // }
+            // else if(){
+            //     $Frecuencia = "treinta";
+            // }
+
+            $this->vista("paginas/graficos_V", $Datos);
         }
     }
 ?>
