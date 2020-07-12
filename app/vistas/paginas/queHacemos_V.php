57808c3c70b43759d87dff9b6fad07815c2af5b0
PabloCabeza <pcabeza7@gmail.com>
2020-07-12 04:52:56 -0400

    Se editó el texto del mensaje

--- app/vistas/paginas/queHacemos_V.php
+++ app/vistas/paginas/queHacemos_V.php
@@ -6,13 +6,11 @@
                 <h1 class="h1_2">¿Porque hacemos y trabajamos en Ciudad Transparente?</h1>
                 <p class="p_1">El tiempo nos ha demostrado con mucho dolor y padecimiento, que los politicos venezolanos acostumbran a negociar las instituciones publicas, administran los recursos economicos de la nación en la oscuridad, permiten la degradación de los recursos naturales, violan las leyes y ocultan los hechos, y disfrazan las malas practicas con la compra de voluntades, el populismo, las regalias, el marketing y la psicologia de la manipulación.</p>
 
-                <p class="p_1">Partiendo de estas caracteristicas generales, como podemos esperar que los politicos resuelvan los problemas del pais, cuando en realidad son parte del problema. Teniendo en cuenta que el desarrollo es obra de grupos de personas organizadas que colocan voluntad, inteligencia, motivación, tenacidad y creatividad en las actividades que emprenden, deberíamos dejar de escuchar promesas politicas particulares y procurar trabajar juntos en nuestras soluciones.</p>
+                <p class="p_1">Partiendo de estas caracteristicas generales, estos personajes en realidad son el inicio y la continuidad de nuestro subdesarrollo, teniendo en cuenta que el desarrollo es obra de grupos de personas organizadas que colocan voluntad, inteligencia, motivación, tenacidad y creatividad en las actividades que emprenden, deberíamos dejar de escuchar promesas politicas y procurar trabajar juntos en nuestras soluciones.</p>
 
-                <p class="p_1">En "Ciudad Transparente" queremos cambiar este ciclo nocivo y encaminarmos hacia un nuevo orden social, donde todos controlemos como van las cuentas y los hechos, que el trabajo publico se haga puertas abiertas; de allí que iniciamos con esta plataforma, que captura las inconformidades de los usuarios de los servicios publicos  para generar reportes estadisticos, historiales de fallas, datos abiertos, xxxxxxxxx y lo más importante, en un momento determinado utilizando tecnicas de Machine learning y Data science pudiesemos prevenir posibles actos de corrupción y fraude a la nación.</p>
+                <p class="p_1">En "Ciudad Transparente" queremos cambiar este ciclo nocivo en el que actualmente vivimos y encaminarmos hacia un nuevo orden social con las intenciones reales de lograrlo, esta plataforma es el inicio de una serie de contrapropuestas radicales, modernas e innovadoras que el movimiento se trae entre manos.</p>
 
-                <p class="p_1">Recuerde, deje de creer que un politico (socialista o capitalista), los marines norteamericanos, los rusos, los iraquies o los chinos van a solucionar nuestros problemas sociales; es usted quién debe sumarse al trabajo e involucrarse en la contraloría social, dejar la dependencia, el interes, el miedo o en otros muchos casos dejar de darsela de vivaracho y oportunista. </p>
-                <div class="contenedor_22">
-                </div>
+                <p class="p_1">Nuestro próximo pequeño paso depende mucho de tí, conoce los detalles. <a class="boton_1"  href="<?php echo RUTA_URL . '/NuestroADN_C/pablins_ky';?>">Ver</a></p>
             </div> 
         </div>
 
