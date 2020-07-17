bfbdaac3460a12409443c7105d54b407cbaf2e09
PabloCabeza <pcabeza7@gmail.com>
2020-07-16 21:15:23 -0400

    Se editó el texto de la página

--- app/vistas/paginas/donar_V.php
+++ app/vistas/paginas/donar_V.php
@@ -4,9 +4,7 @@
 	<div class="container contenedor_1">
         <div class="contenedor_15">
             <h1 class="h1_2">Como sostenemos el proyecto.</h1>
-            <p>Ciudad transparente es una ONG que sostiene su funcionamiento por medio de donaciones, se estan estudiando otros modos de financiamiento como la elaboración de trabajos en materia de metricas y estadisticas, sin embargo el tiempo, el tamaño del equipo y el trabajo de desarrolo de la plataforma no nos ha permitido abrirnos camino a la independencia economica.</p>
-            <p>Si deseas donar al proyecto puedes realizarlo aportando a la cuenta bancaria Nº</p>
-            <p>Si deseas hacer tu donativo en especies, involucrandote en el proyecto y hacerlo crecer puedes contactarnos por los siguientes medios:</p>
+            
         </div>
     </div>
 
